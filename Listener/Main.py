03c48f74037020b994c2c05501dd7b9f3677de8a
rubenhortas <rubenhortas@gmail.com>
2015-10-03 01:36:17 +0200

    Added interpreter to the comments.

--- Listener/Main.py
+++ Listener/Main.py
@@ -2,20 +2,22 @@
 # _*_ coding:utf-8 _*_
 
 """
-@author:    Rubén Hortas Astariz <http://rubenhortas.blogspot.com>
-@contact:   rubenhortas at gmail.com
-@github:    http://github.com/rubenhortas
-@license:   CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
-@file:      Main.py
+@author:        Rubén Hortas Astariz <http://rubenhortas.blogspot.com>
+@contact:       rubenhortas at gmail.com
+@github:        http://github.com/rubenhortas
+@license:       CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
+@file:          Main.py
+@interpreter:   2.7
 """
-
-from Subject import Subject
-from ListenerClass import ListenerClass
-from IListener import IListener
 
 """
 Example of Listener Pattern implementation.
 """
+from IListener import IListener
+from ListenerClass import ListenerClass
+from Subject import Subject
+
+
 if __name__ == "__main__":
     print("main")
     subject = Subject(ListenerClass())
