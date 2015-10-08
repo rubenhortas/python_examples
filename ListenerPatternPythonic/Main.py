e4abcd75cdbcb5cf164c99265bf3baede6b8398b
rubenhortas <rubenhortas@gmail.com>
2015-10-08 23:43:26 +0200

    Added Listener Pattern implemented in a pythonic way.

---
+++ ListenerPatternPythonic/Main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*
+
+"""
+@author:      Rubén Hortas Astariz <http://rubenhortas.blogspot.com>
+@contact:     rubenhortas at gmail.com
+@github:      http://github.com/rubenhortas
+@license:     CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
+@file:        Main.py
+@interpreter: python3
+"""
+
+import ListenerClass
+import Subject
+
+
+if __name__ == "__main__":
+    print("main")
+    subject = Subject(ListenerClass())
