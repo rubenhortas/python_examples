41baaa21ba2793c4f3805ab0cd508dd0668458b0
rubenhortas <rubenhortas@gmail.com>
2015-10-17 22:32:43 +0200

    Added example of passing default arguments to functions.

---
+++ DefaultArguments/DefaultArguments.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*
+
+"""
+@author:      Rubén Hortas Astariz <http://rubenhortas.blogspot.com>
+@contact:     rubenhortas at gmail.com
+@github:      http://github.com/rubenhortas
+@license:     CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
+@file:        DefaultArguments/DefaultArguments.py
+@interpreter: python3
+"""
+
+
+def function(optionalArgument=True):
+    if(optionalArgument == True):
+        print("Ok, it's true")
+    else:
+        print("It's false")
+
+
+if __name__ == "__main__":
+    function()
+    function(False)
