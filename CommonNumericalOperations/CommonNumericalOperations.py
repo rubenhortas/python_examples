adf18c2c7de405d7ce5260a8e839f22b1d761dc7
rubenhortas <rubenhortas@gmail.com>
2015-10-18 11:42:36 +0200

    Added Common Numerical Operations examples

---
+++ CommonNumericalOperations/CommonNumericalOperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*
+
+"""
+@author:      Rubén Hortas Astariz <http://rubenhortas.blogspot.com>
+@contact:     rubenhortas at gmail.com
+@github:      http://github.com/rubenhortas
+@license:     CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
+@file:        CommonNumericalOperations/CommonNumericalOperations.py
+@interpreter: python3
+"""
+
+if __name__ == '__main__':
+    print(11 / 2)  # Division
+    print(11 // 2)  # Integer division. Rounds down to the nearest integer.
+    print(-11 // 2)
+    print(11.0 // 2)  # Returns a float (rounded down to the nearest integer)
+    # because the first argument is a float.
+    print(11**2)  # Raise to the power of
+    print(11 % 2)  # Returns the remainder
