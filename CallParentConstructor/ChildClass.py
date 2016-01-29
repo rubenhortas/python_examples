d264acdfbef7dfe92d320a56d7a8a2f764a5272c
rubenhortas <rubenhortas@gmail.com>
2016-01-30 00:50:27 +0100

    Added Call Parent Constructor example.

---
+++ CallParentConstructor/ChildClass.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*
+
+"""
+@author:      Rubén Hortas Astariz <http://rubenhortas.blogspot.com>
+@contact:     rubenhortas at gmail.com
+@github:      http://github.com/rubenhortas
+@license:     CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
+@file:        ChildClass.py
+@interpreter: python3
+"""
+
+from ParentClass import Parent
+
+
+class Child(Parent):
+
+    def ___intit__(self):
+        super(Parent, self).__init__()
---
+++ CallParentConstructor/ParentClass.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*
+
+"""
+@author:      Rubén Hortas Astariz <http://rubenhortas.blogspot.com>
+@contact:     rubenhortas at gmail.com
+@github:      http://github.com/rubenhortas
+@license:     CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
+@file:        ParentClass.py
+@interpreter: python3
+"""
+
+
+# Parent class has to inherit from "object" class
+class Parent(object):
+
+    def __init__(self):
+        print("parent constructor called!")
