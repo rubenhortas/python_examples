b3fa50a74ed345f3187286cd53cab348f621b5f2
rubenhortas <rubenhortas@gmail.com>
2019-05-24 20:52:41 +0200

    Updated for ms vs

---
+++ Strings/StringExamples.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+"""
+@author:        Rubén Hortas Astariz <http://rubenhortas.blogspot.com>
+@contact:       rubenhortas at gmail.com
+@github:        http://github.com/rubenhortas
+@license:       CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
+@file:          StringExamples.py
+@interpreter:   python 2.7
+"""
+
+if __name__ == '__main__':
+
+    # Triple quotes allow printing in several lines with a single instruction
+    print("""Hello
+    world""")
+    print
+
+    print('''Hello
+    world''')
+    print
+
+    # Return a copy of the string with only its first character capitalized
+    str = "hello world"
+    print(str.capitalize())
+    print
