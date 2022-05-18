cd04b56e8304fa91d2b21594dfb47a00d3d16a8a
rubenhortas <rubenhortas@gmail.com>
2022-05-18 21:07:35 +0200

    Fixed comments and pep8

--- Base64/Base64Example.py
+++ Base64/Base64Example.py
@@ -6,16 +6,13 @@ import base64
 
 if __name__ == '__main__':
     message = 'Hello world!'
-    print('message: %s' % str(message))  # str(message) to remove b' from string output
+    print('message: %s' % message)
 
     # Encode
     message_bytes = message.encode('ascii')
-    base64_message = base64.b64encode(message_bytes).decode()  # .decode() to remove b' from string output
-    print('base64_message: %s' % str(base64_message))
+    base64_message = base64.b64encode(message_bytes).decode()  # .decode() to convert from bytes to string
+    print('base64_message: %s' % base64_message)
 
     # Decode
-    decoded_message = base64.b64decode(base64_message).decode()  # .decode() to remove b' from string output
+    decoded_message = base64.b64decode(base64_message).decode()   # .decode() to convert from bytes to string
     print('decoded_message: %s' % decoded_message)
-
-
-
