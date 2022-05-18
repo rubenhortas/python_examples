c643bd3b3bcf6f527eb5119d6b9ebe7384800368
rubenhortas <rubenhortas@gmail.com>
2022-05-18 21:08:43 +0200

    Renamed variables

--- TernaryOperators/TernaryOperators.py
+++ TernaryOperators/TernaryOperators.py
@@ -3,14 +3,14 @@
 
 if __name__ == '__main__':
     condition = True
-    condition_true = 'Condition is true'
-    condition_false = 'Condition is false'
+    message_condition_true = 'Condition is true'
+    message_condition_false = 'Condition is false'
 
     # First way
-    result = condition_true if condition else condition_false
+    result = message_condition_true if condition else message_condition_false
     print(result)
 
     # Second way (Less intuitive, inmo)
     # (if_condition_is_false, if_condition_is_true)[condition]
-    result = (condition_false, condition_true)[condition]
+    result = (message_condition_false, message_condition_true)[condition]
     print(result)
