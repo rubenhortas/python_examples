89bf1f3c768ea5ac49870ae090f39d74b8adc619
rubenhortas <rubenhortas@gmail.com>
2022-05-20 18:20:19 +0200

    Added new example

--- StringConcatenationMeasureEfficiency/StringConcatenationMeasureEfficiency.py
+++ StringConcatenationMeasureEfficiency/StringConcatenationMeasureEfficiency.py
@@ -42,6 +42,13 @@ def string_format():
         str_out = "{0}{1}".format(str_out, str(i))
 
 
+def string_format_without_positional_arguments():  # >= 3.1
+    str_out = ""
+
+    for i in range(1, ITEM_NUMBERS):
+        str_out = "{}{}".format(str_out, str(i))
+
+
 def character_array():
     char_array = array("b")
 
@@ -81,6 +88,9 @@ if __name__ == '__main__':
     print("Concatenating with string format...")
     average_times.append(measure_time(string_format))
 
+    print("Concatenating with string format without positional arguments...")
+    average_times.append(measure_time(string_format_without_positional_arguments))
+
     print("Concatenating with character array...")
     average_times.append(measure_time(character_array))
 
@@ -105,6 +115,7 @@ if __name__ == '__main__':
         2 - write_pseudo_file
         3 - naive_appending
         4 - format_specifiers
-        5 - string_format
-        6 - character_array
+        5 - string_format_without_positional_arguments
+        6 - string_format
+        7 - character_array
     """
