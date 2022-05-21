ddf4b2fba34fa40ed50937c099ed3fed67d3d79b
rubenhortas <rubenhortas@gmail.com>
2022-05-21 12:58:37 +0200

    Improved comment

--- CommonNumericalOperations/CommonNumericalOperations.py
+++ CommonNumericalOperations/CommonNumericalOperations.py
@@ -5,8 +5,7 @@ if __name__ == '__main__':
     print(11 / 2)  # Division
     print(11 // 2)  # Integer division. Rounds down to the nearest integer.
     print(-11 // 2)
-    print(11.0 // 2)  # Returns a float (rounded down to the nearest integer)
-    # because the first argument is a float.
+    print(11.0 // 2)  # Returns a float (rounded down to the nearest integer) because the first argument is a float.
     print(11**2)  # Raise to the power of
     print(11 % 2)  # Returns the remainder
 
