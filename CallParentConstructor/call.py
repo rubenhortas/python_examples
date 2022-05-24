af04052df11f75268c3ea735cce927e4e267590f
rubenhortas <rubenhortas@gmail.com>
2022-05-24 19:20:36 +0200

    Renamed packages to accomplish with pep8

--- CallParentConstructor/call.py
+++ CallParentConstructor/call.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # _*_ coding:utf-8 _*
-from ChildClass import Child
+from childClass import Child
 
 if __name__ == '__main__':
     new_class = Child()
--- CallParentConstructor/childClass.py
+++ CallParentConstructor/childClass.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # _*_ coding:utf-8 _*
-from ParentClass import Parent
+from parentClass import Parent
 
 
 class Child(Parent):
