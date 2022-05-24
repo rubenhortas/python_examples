896584fa66a61cc3c14f7b36bee1651191e65411
rubenhortas <rubenhortas@gmail.com>
2022-05-24 20:04:06 +0200

    Added bolean examples

---
+++ booleans_example/booleans_example.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+
+    # Due to some legacy issues left over from Python 2, booleans can be treated as numbers. True is 1; False is 0.
+    print(True + True)  # 2
+    print(False + False)  # 0
+    print(True - False)  # 1
+    print(True * False)  # 0
