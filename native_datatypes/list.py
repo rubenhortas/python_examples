5687af0ef36b1f13ddc1e685877485f30e2a36ce
rubenhortas <rubenhortas@gmail.com>
2022-05-28 22:18:25 +0200

    Refactored

--- native_datatypes/list.py
+++ native_datatypes/list.py
@@ -1,61 +1,99 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
-    lst = []
+def add_items(lst):
+    print("Adding items")
 
-    # Adding items
     lst = lst + ["a"]
     lst.append(1)  # In a lst items don"t need to be the same type
-    lst.extend(["b", 2])
+    lst.extend(["a", 2])
     lst.insert(0, True)
 
-    print(f"List: {lst}")
-
-    # Searching for values
-    print("Searching for values")
-    if 'a' in lst:
-        print("\t'a' is in the list")
-
-    lst.append("a")
     print(f"\tList: {lst}")
 
-    position = lst.index("a")  # Returns the position of the first occurrence
-    print(f"\tPosition: {position}")
+    return lst
 
-    # noinspection PyBroadException
+
+def search(lst, value):
+    # Searching for values
+    print("Searching for values")
+
+    if value in lst:
+        print(f"\t{value} is in the list")
+
+    print(f"\tList: {lst}")
+
+
+def get_value_position(lst, value):
+    print(f"Getting value position in {lst}:")
+
     try:
-        # A negative index accesses items from the end of the list counting backwards.
-        # Negative index are valid, so raises an exception if the item is not
-        # found
-        print(f"\tItem in -1 position: {lst[-1]}")
-        position = lst.index(False)
-    except:
-        print("\tItem not found")
+        print(f"\tPosition of {value}: {item_list.index(value)}")  # Returns the position of the first occurrence
+    except ValueError:
+        print(f"\t{value} not found")
 
-    # Popping
-    print("Popping")
+
+def get_value_by_position(lst, position):
+    # A negative index accesses items from the end of the list counting backwards.
+    # Negative index are valid, so raises an exception if the item is not found.
+    print(f"Getting value in position {position} in {lst}:")
+
+    try:
+        print(f"\tItem in {position}: {lst[position]}")
+    except IndexError:
+        print(f"\tPosition {position} not found")
+
+
+def popping(lst):
+    print("Popping:")
     print(f"\t{lst}")
+
     lst.pop()
+
     print(f"\t{lst}")
 
-    # Lists in a boolean context
+
+def check_empty(lst):
     # Empty lists are false otherwise is true
-    empty_list = []
     print("Lists in a boolean context:")
+
     if lst:
         print(f"\t{lst} is not empty")
+    else:
+        print(f"\t{lst} is empty")
 
-    if not empty_list:
-        print(f"\t{empty_list} is empty")
 
-    # List comprehensions
+def comprehensions():
     # [ expression for item in list if conditional ]
+    print("List comprehensions:")
+
     numbers = [i for i in range(0, 10)]
     even_numbers = [i for i in range(0, 10) if i % 2 == 0]
-    print("List comprehensions:")
+
     print(f"\tnumbers: {numbers}")
     print(f"\teven numbers: {even_numbers}")
 
-    # Reversing list
+
+def reversing():
+    numbers = [i for i in range(0, 10)]
     reversed_numbers = numbers[::-1]
+
     print(f"Reversing: {numbers} backwards is {reversed_numbers}")
+
+
+if __name__ == '__main__':
+    item_list = []
+    empty_list = []
+
+    print(f"List: {item_list}")
+
+    item_list = add_items(item_list)
+    search(item_list, 'a')
+    get_value_position(item_list, 'a')
+    get_value_position(item_list, 'non_existing_value')
+    get_value_by_position(item_list, -1)
+    get_value_by_position(empty_list, -1)
+    popping(item_list)
+    check_empty(item_list)
+    check_empty(empty_list)
+    comprehensions()
+    reversing()
