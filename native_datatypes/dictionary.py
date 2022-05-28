b730ac8b65fb4958df7785208871413f550ca513
rubenhortas <rubenhortas@gmail.com>
2022-05-28 21:15:33 +0200

    Refactored

--- native_datatypes/dictionary.py
+++ native_datatypes/dictionary.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
-    users = {
-        "root": "r00t.h4x0r.p455",
-        "guest": "",
-        "bob": "123456",
-        "alice": "qwerty"
-    }
+def initialize_users():
+    return users
 
-    print(f"Passwords: {users}")
 
+def add_users(users):
+    # Modify dictionary values
     users["bob"] = "temporaryPassword"
     bob_password = users["bob"]
     print(f"New bob password: {bob_password}")
@@ -18,21 +14,39 @@ if __name__ == '__main__':
     users["Bob"] = "temporary2"
     print(f"Updated Bob password: {users}")  # We have a new Bob
 
+
+def check_empty(users, empty):
     # Dictionaries in a boolean context
     # An empty dictionary is False otherwise is true
-    empty_dictionary = {}
-
     if users:
         print(f"{users} is not empty")
 
-    if not empty_dictionary:
+    if not empty:
         print(f"{empty_dictionary} is empty")
 
+
+def merge(users, new_users):
     # Merging dictionaries Python >= 3.5
+    merged_users = {**users, **new_users}
+
+    print(f"New users: {new_users}")
+    print(f"Merged users: {merged_users}")
+
+
+if __name__ == '__main__':
+    empty_dictionary = {}
+    users = {
+        "root": "r00t.h4x0r.p455",
+        "guest": "",
+        "bob": "123456",
+        "alice": "qwerty"
+    }
     new_users = {
         "sysadmin": "r2@l.r00t",
         "anonymous": "anonymous"
     }
 
-    merged_users = {**users, **new_users}
-    print(f"Merged users: {merged_users}")
+    print(f"Passwords: {users}")
+
+    check_empty(users, empty_dictionary)
+    merge(users, new_users)
