bba76f952a7e3332a7eaaa49f15d2376f43e78a9
rubenhortas <rubenhortas@gmail.com>
2022-05-30 19:47:16 +0200

    Deleted blank line

--- native_datatypes/boolean.py
+++ native_datatypes/boolean.py
@@ -1,5 +1,4 @@
 if __name__ == '__main__':
-
     # Due to some legacy issues left over from Python 2, booleans can be treated as numbers. True is 1; False is 0.
     print(True + True)  # 2
     print(False + False)  # 0
