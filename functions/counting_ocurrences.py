7a6ca06bcabed411f8449631df478aac67958dc7
rubenhortas <rubenhortas@gmail.com>
2022-06-01 19:37:52 +0200

    Added example

---
+++ functions/counting_ocurrences.py
@@ -0,0 +1,6 @@
+# You can use Counter from the collections library to get a dictionary with counts of all the unique elements in a list
+from collections import Counter
+
+if __name__ == '__main__':
+    lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+    print(f"Lorem ipsum counter: {Counter(lorem_ipsum)}")
