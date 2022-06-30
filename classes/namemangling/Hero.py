1c7fe0d3acc37f0e86aba7d86472383674a5c757
rubenhortas <rubenhortas@gmail.com>
2022-06-30 18:55:03 +0200

    Improved

--- classes/namemangling/Hero.py
+++ classes/namemangling/Hero.py
@@ -1,14 +1,14 @@
 class Hero:
-    hero_name = None
-    real_name = None
+    __hero_name = None
+    __real_name = None
 
     def __init__(self, hero_name, real_name):
-        self.hero_name = hero_name
-        self.real_name = real_name
+        self.__hero_name = hero_name
+        self.__real_name = real_name
 
     def print_name(self):
-        print(f'I\'m {self.hero_name}!')
+        print(f'I\'m {self.__hero_name}!')
 
     # Class privates starts with two underscores
     def __print_real_name(self):
-        print(f'PWNED! I\'m {self.real_name} :(')
+        print(f'PWNED! I\'m {self.__real_name} :(')
