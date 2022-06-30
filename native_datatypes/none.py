14952a2257dd55126876bfa8ecfc07173c3a09e5
rubenhortas <rubenhortas@gmail.com>
2022-06-30 19:16:37 +0200

    Improved

--- native_datatypes/none.py
+++ native_datatypes/none.py
@@ -4,16 +4,16 @@
 # It has its own datatype (NoneType)
 # You can assign None to any variable, but you can not create other NoneType objects
 
-if __name__ == '__main__':
-    var_none = None
-    var_none2 = None
+CTE_NONE1 = None
+CTE_NONE2 = None
 
+if __name__ == '__main__':
     print("None:")
     print(f"None == False -> {None == False}")  # False
     print(f"None == 0 -> {None == 0}")  # False
     print(f"None == '' -> {None == ''}")  # False
-    print(f"None == var_none -> {None == var_none}")  # True
-    print(f"var_none == var_none2 -> {var_none == var_none2}")  # True
+    print(f"None == var_none -> {None == CTE_NONE1}")  # True
+    print(f"var_none == var_none2 -> {CTE_NONE1 == CTE_NONE2}")  # True
 
     # In a boolean context None is False
     if not None:
