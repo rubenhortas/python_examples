d4aba6331595fb7e1a205f98d0a1ef14a58151e6
rubenhortas <rubenhortas@gmail.com>
2022-06-30 19:07:55 +0200

    Improved

--- cryptography/base64_example.py
+++ cryptography/base64_example.py
@@ -2,13 +2,13 @@
 
 import base64
 
+MESSAGE = "Hello world!"
 
 if __name__ == '__main__':
-    message = "Hello world!"
-    print(f"message: {message}")
+    print(f"message: {MESSAGE}")
 
     # Encode
-    message_bytes = message.encode('ascii')
+    message_bytes = MESSAGE.encode('ascii')
     base64_message = base64.b64encode(message_bytes).decode()  # .decode() to convert from bytes to string
     print(f"base64_message: {base64_message}")
 
