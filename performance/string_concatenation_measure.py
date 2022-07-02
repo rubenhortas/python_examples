c9ac47a5eae9a894056c2b7f82bb05a2c91fbf2e
rubenhortas <rubenhortas@gmail.com>
2022-07-02 13:23:11 +0200

    Add measures with functools

--- performance/string_concatenation_measure.py
+++ performance/string_concatenation_measure.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 
-from io import StringIO
+import time
 from array import array
-import timeit
+from functools import wraps
+from io import StringIO
 
 ITEM_NUMBERS = 10000
 ITERATIONS = 1000
 
 
-def measure_time(fun):
-    t_start = timeit.default_timer()
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper():
+        start_time = time.perf_counter()
 
-    for i in range(1, ITERATIONS):
-        fun()
+        for i in range(1, ITERATIONS):
+            func()
 
-    t_end = timeit.default_timer()
-    t_total = (t_end - t_start) / ITERATIONS
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
 
-    return f"{t_total:.10f} {fun.__name__}"
+        return func.__name__, total_time
+
+    return timeit_wrapper
 
 
+@timeit
 def naive_appending():
     str_out = ""
 
@@ -27,14 +33,15 @@ def naive_appending():
         str_out = str_out + str(i)
 
 
+@timeit
 def format_specifiers():
-    # str_out = "%s %s" % (str_hello, str_my_friend)
     str_out = ""
 
     for i in range(1, ITEM_NUMBERS):
         str_out = "%s%s" % (str_out, str(i))
 
 
+@timeit
 def string_format():
     str_out = ""
 
@@ -42,6 +49,7 @@ def string_format():
         str_out = "{0}{1}".format(str_out, str(i))
 
 
+@timeit
 def string_format_without_positional_arguments():  # python >= 3.1
     str_out = ""
 
@@ -49,6 +57,7 @@ def string_format_without_positional_arguments():  # python >= 3.1
         str_out = "{}{}".format(str_out, str(i))
 
 
+@timeit
 def character_array():
     char_array = array("b")
 
@@ -56,6 +65,7 @@ def character_array():
         char_array.frombytes(bytes(i))
 
 
+@timeit
 def build_list():
     strings = []
 
@@ -65,6 +75,7 @@ def build_list():
     "".join(strings)
 
 
+@timeit
 def write_pseudo_file():
     file_str = StringIO()
 
@@ -74,47 +85,53 @@ def write_pseudo_file():
 
 
 if __name__ == '__main__':
-    average_times = []
+    results = {}
     pos = 1
 
     print("Starting measurements...")
+
     print("Concatenating with naive appending...")
-    average_times.append(measure_time(naive_appending))
+    function_name, execution_time = naive_appending()
+    results[function_name] = execution_time
 
     print("Concatenating with format specifiers...")
-    average_times.append(measure_time(format_specifiers))
+    function_name, execution_time = format_specifiers()
+    results[function_name] = execution_time
 
     print("Concatenating with string format...")
-    average_times.append(measure_time(string_format))
+    function_name, execution_time = string_format()
+    results[function_name] = execution_time
 
     print("Concatenating with string format without positional arguments...")
-    average_times.append(measure_time(string_format_without_positional_arguments))
+    function_name, execution_time = string_format_without_positional_arguments()
+    results[function_name] = execution_time
 
     print("Concatenating with character array...")
-    average_times.append(measure_time(character_array))
+    function_name, execution_time = character_array()
+    results[function_name] = execution_time
 
     print("Concatenating with build list...")
-    average_times.append(measure_time(build_list))
+    function_name, execution_time = build_list()
+    results[function_name] = execution_time
 
     print("Concatenating with write pseudo file...")
-    average_times.append(measure_time(write_pseudo_file))
+    function_name, execution_time = write_pseudo_file()
+    results[function_name] = execution_time
 
     print("Results:")
-    sorted_averaged_times = sorted(average_times)
+    sorted_results = sorted(results.items(), key=lambda item: item[1], reverse=False)
 
-    for average_time in sorted_averaged_times:
-        print(f"\t{pos} - {average_time}")
+    for result in sorted_results:
+        print(f"\t{pos} - {result[0]} {result[1]:.10f} secs")
         pos = pos + 1
-
-    print()
 
     """"
     My results (sorted from faster to slowest):
-        1 - build_list
-        2 - write_pseudo_file
-        3 - naive_appending
-        4 - format_specifiers
-        5 - string_format_without_positional_arguments
-        6 - string_format
-        7 - character_array
+       1 - naive_appending
+       2 - build_list
+       3 - write_pseudo_file
+       4 - format_specifiers
+       5 - string_format_without_positional_arguments
+       6 - string_format
+       7 - character_array
     """
