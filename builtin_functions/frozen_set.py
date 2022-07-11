d558e86085d02d78900d45269b31b2505629d4d0
rubenhortas <rubenhortas@gmail.com>
2022-07-11 19:44:04 +0200

    Added frozenset

---
+++ builtin_functions/frozen_set.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# The frozenset() is an inbuilt function in Python which takes an iterable object as input and makes them immutable.
+# Simply it freezes the iterable objects and makes them unchangeable.
+
+if __name__ == '__main__':
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    frozen_numbers = frozenset(numbers)
+
+    try:
+        # If you want to change the frozenset an exception will raise
+        frozen_numbers[1] = 0
+    except TypeError as ex:
+        print(ex)
