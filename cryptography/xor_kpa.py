87178636844e0e81d27f8b3c45c6f10463d2d392
rubenhortas <rubenhortas@gmail.com>
2022-08-18 20:54:40 +0200

    Updated

--- cryptography/xor_kpa.py
+++ cryptography/xor_kpa.py
@@ -3,17 +3,18 @@
 import os
 
 KEY_LENGTH = 4
+ENCODING = 'UTF-8'
 
 
 # noinspection PyShadowingNames
 def initialize():
     print("Initializing values...")
 
-    plain_text_string = "Hello world!".encode('utf-8')  # string to bytes
-    xor_key = os.urandom(KEY_LENGTH)  # Random KEY_LENGTH bytes key
+    plain_text_string = "Hello world!".encode(ENCODING)  # string to bytes
+    xor_key = os.urandom(KEY_LENGTH)  # Random KEY_LENGTH bytes KEY
     xor_string = xor(xor_key, plain_text_string)
 
-    print(f"\tplain_text_string: {plain_text_string.decode('utf-8')}")
+    print(f"\tplain_text_string: {plain_text_string.decode(ENCODING)}")
     print(f"\txor_key: {xor_key}")
     print(f"\tciphertext: {xor_string}")
     print()
@@ -34,12 +35,12 @@ def xor(key: bytes, data: bytes) -> bytes:
 def get_xor_key(data: bytes, cipher_data: bytes) -> bytes:
     key = b''
 
-    print("Guessing the secret key using for xor...")
+    print("Guessing the secret KEY using for xor...")
 
     for i in range(KEY_LENGTH):
         key += bytes([data[i] ^ cipher_data[i]])
 
-    print(f"\tGuessed key: {key}")
+    print(f"\tGuessed KEY: {key}")
     print()
 
     return key
@@ -50,20 +51,20 @@ if __name__ == '__main__':
     plain_text_string, key, ciphertext = initialize()
 
     # If we have the initial plain text string (or at least a part) and the result ciphertext
-    # we can guess the key used for cipher the string.
+    # we can guess the KEY used for cipher the string.
     guessed_key = get_xor_key(plain_text_string, ciphertext)
 
-    # If we have guessed the key we can guess the initial string
+    # If we have guessed the KEY we can guess the initial string
     guessed_plain_text_string = xor(guessed_key, ciphertext)
     
     # Check the results
     print("Results:")
     print("\tKeys:")
-    print(f"\t\tRandom generated key: {key}")
-    print(f"\t\tGuessed key: {guessed_key}")
+    print(f"\t\tRandom generated KEY: {key}")
+    print(f"\t\tGuessed KEY: {guessed_key}")
     print(("\t\tKeys are different", "\t\tKeys are equals")[key == guessed_key])  # keys are equal
     print()
     print("Initial strings:")
-    print(f"\t\tInitial plain text string: {plain_text_string.decode('utf-8')}")
-    print(f"\t\tGuessed initial plain text string: {guessed_plain_text_string.decode('utf-8')}")
+    print(f"\t\tInitial plain text string: {plain_text_string.decode(ENCODING)}")
+    print(f"\t\tGuessed initial plain text string: {guessed_plain_text_string.decode(ENCODING)}")
     print(("\t\tStrings are different", "\t\tStrings are equals")[plain_text_string == guessed_plain_text_string])  # strings are equal
