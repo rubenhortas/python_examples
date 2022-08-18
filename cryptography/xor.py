9462df034a0265a5a46a34994b525d7168288ef0
rubenhortas <rubenhortas@gmail.com>
2022-08-18 20:54:53 +0200

    Added

---
+++ cryptography/xor.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+KEY = b'123456'
+PLAINTEXT_STRING = b'hello world!'
+ENCODING = 'UTF-8'
+
+
+def xor(key: bytes, data: bytes) -> bytes:
+    xor_data = b''
+
+    for i in range(len(data)):
+        xor_data += bytes([data[i] ^ key[i % len(key)]])
+    return xor_data
+
+
+if __name__ == '__main__':
+    xored_string = xor(KEY, PLAINTEXT_STRING)
+    unxored_string = xor(KEY, xored_string)
+
+    print('Plaintext string: "' + PLAINTEXT_STRING.decode(ENCODING) + '"')
+    print('Xored string: "' + xored_string.decode(ENCODING) + '"')
+    print('Unxored string: "' + unxored_string.decode(ENCODING) + '"')
