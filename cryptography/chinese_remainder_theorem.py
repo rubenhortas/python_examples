dd9f74df69220827970cd82b440498eaf721b09e
rubenhortas <rubenhortas@gmail.com>
2022-10-25 18:53:17 +0200

    Updated

--- cryptography/chinese_remainder_theorem.py
+++ cryptography/chinese_remainder_theorem.py
@@ -3,9 +3,9 @@ from sympy.ntheory.modular import crt
 # n mod(3) = 2
 # n mod(5) = 3
 # n mod(7) = 2
-N = [3, 5, 7]  # Modulus
+M = [3, 5, 7]  # Modulus
 R = [2, 3, 2]  # Results
 
 if __name__ == '__main__':
-    crt_result = crt(N, R)[0]
-    print(f'Result of the Chinese Remainder Theorem = {crt_result}')
+    crt_result = crt(M, R)[0]
+    print(f'Result of the Chinese Remainder Theorem = {crt_result}')  # = 23
