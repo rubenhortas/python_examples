a8b572d81d7d2b89c1cfc9f9b82f62626bafcd3f
rubenhortas <rubenhortas@gmail.com>
2022-10-28 19:04:48 +0200

    Updated

--- cryptography/xor.py
+++ cryptography/xor.py
@@ -4,17 +4,17 @@ KEY = b'123456'
 PLAINTEXT_STRING = b'hello world!'
 
 
-def xor(key: bytes, data: bytes) -> bytes:
-    xor_data = b''
+def __xor(key: bytes, data: bytes) -> bytes:
+    xored_data = b''
 
     for i in range(len(data)):
-        xor_data += bytes([data[i] ^ key[i % len(key)]])
-    return xor_data
+        xored_data += bytes([data[i] ^ key[i % len(key)]])
+    return xored_data
 
 
 if __name__ == '__main__':
-    xored_data = xor(KEY, PLAINTEXT_STRING)
-    unxored_data = xor(KEY, xored_data)
+    xored_data = __xor(KEY, PLAINTEXT_STRING)
+    unxored_data = __xor(KEY, xored_data)
 
     print('Plaintext string: "' + PLAINTEXT_STRING.decode() + '"')
     print('Xored string: "' + xored_data.decode() + '"')
--- cryptography/xor_kpa.py
+++ cryptography/xor_kpa.py
@@ -6,12 +6,12 @@ KEY_LENGTH = 4
 
 
 # noinspection PyShadowingNames
-def initialize():
+def __initialize():
     print("Initializing values...")
 
     plain_text_string = "Hello world!".encode()  # string to bytes
     xor_key = os.urandom(KEY_LENGTH)  # Random KEY_LENGTH bytes KEY
-    xor_string = xor(xor_key, plain_text_string)
+    xor_string = __xor(xor_key, plain_text_string)
 
     print(f"\tplain_text_string: {plain_text_string.decode()}")
     print(f"\txor_key: {xor_key}")
@@ -22,16 +22,16 @@ def initialize():
 
 
 # noinspection PyShadowingNames
-def xor(key: bytes, data: bytes) -> bytes:
-    xor_data = b''
+def __xor(key: bytes, data: bytes) -> bytes:
+    xored_data = b''
 
     for i in range(len(data)):
-        xor_data += bytes([data[i] ^ key[i % len(key)]])
-    return xor_data
+        xored_data += bytes([data[i] ^ key[i % len(key)]])
+    return xored_data
 
 
 # noinspection PyShadowingNames
-def get_xor_key(data: bytes, cipher_data: bytes) -> bytes:
+def __get_xor_key(data: bytes, cipher_data: bytes) -> bytes:
     key = b''
 
     print("Guessing the secret KEY using for xor...")
@@ -47,14 +47,14 @@ def get_xor_key(data: bytes, cipher_data: bytes) -> bytes:
 
 # Example of how it works and how to perform a Known-plaintext attack (KPA) on a xor ciphertext
 if __name__ == '__main__':
-    plain_text_string, key, ciphertext = initialize()
+    plain_text_string, key, ciphertext = __initialize()
 
     # If we have the initial plain text string (or at least a part) and the result ciphertext
     # we can guess the KEY used for cipher the string.
-    guessed_key = get_xor_key(plain_text_string, ciphertext)
+    guessed_key = __get_xor_key(plain_text_string, ciphertext)
 
     # If we have guessed the KEY we can guess the initial string
-    guessed_plain_text_string = xor(guessed_key, ciphertext)
+    guessed_plain_text_string = __xor(guessed_key, ciphertext)
     
     # Check the results
     print("Results:")
