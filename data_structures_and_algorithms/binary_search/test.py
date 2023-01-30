4683fe32669b74f6b32baaca6d4de930cb66ac37
rubenhortas <rubenhortas@gmail.com>
2023-01-30 21:46:50 +0100

    Fixed deprecation warning.

--- data_structures_and_algorithms/binary_search/test.py
+++ data_structures_and_algorithms/binary_search/test.py
@@ -10,35 +10,35 @@ class Test(unittest.TestCase):
 
     def test_in_the_middle(self):
         result = locate_position(self.numbers, 4)
-        self.assertEquals(result, 5)
+        self.assertEqual(result, 5)
 
     def test_is_first_element(self):
         result = locate_position(self.numbers, 9)
-        self.assertEquals(result, 0)
+        self.assertEqual(result, 0)
 
     def test_is_last_element(self):
         result = locate_position(self.numbers, 0)
-        self.assertEquals(result, 9)
+        self.assertEqual(result, 9)
 
     def test_is_the_only_number(self):
         result = locate_position([1], 1)
-        self.assertEquals(result, 0)
+        self.assertEqual(result, 0)
 
     def test_not_in_list(self):
         result = locate_position(self.numbers, 11)
-        self.assertEquals(result, -1)
+        self.assertEqual(result, -1)
 
     def test_empty_list(self):
         result = locate_position([], 1)
-        self.assertEquals(result, -1)
+        self.assertEqual(result, -1)
 
     def test_repeated_numbers(self):
         result = locate_position(self.numbers2, 6)
-        self.assertEquals(result, 9)
+        self.assertEqual(result, 9)
 
     def test_several_appearances(self):
         result = locate_position(self.numbers2, 4)
-        self.assertEquals(result, 13)
+        self.assertEqual(result, 13)
 
 
 if __name__ == '__main__':
