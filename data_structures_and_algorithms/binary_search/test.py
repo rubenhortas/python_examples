db22008b339c7ec3d0eaac8402931b848d8488b2
rubenhortas <rubenhortas@gmail.com>
2023-02-01 19:12:17 +0100

    Fixed asserts

--- data_structures_and_algorithms/binary_search/test.py
+++ data_structures_and_algorithms/binary_search/test.py
@@ -10,35 +10,35 @@ class Test(unittest.TestCase):
 
     def test_in_the_middle(self):
         result = locate_position(self.numbers, 4)
-        self.assertEqual(result, 5)
+        self.assertEqual(5, result)
 
     def test_is_first_element(self):
         result = locate_position(self.numbers, 9)
-        self.assertEqual(result, 0)
+        self.assertEqual(0, result)
 
     def test_is_last_element(self):
         result = locate_position(self.numbers, 0)
-        self.assertEqual(result, 9)
+        self.assertEqual(9, result)
 
     def test_is_the_only_number(self):
         result = locate_position([1], 1)
-        self.assertEqual(result, 0)
+        self.assertEqual(0, result)
 
     def test_not_in_list(self):
         result = locate_position(self.numbers, 11)
-        self.assertEqual(result, -1)
+        self.assertEqual(-1, result)
 
     def test_empty_list(self):
         result = locate_position([], 1)
-        self.assertEqual(result, -1)
+        self.assertEqual(-1, result)
 
     def test_repeated_numbers(self):
         result = locate_position(self.numbers2, 6)
-        self.assertEqual(result, 9)
+        self.assertEqual(9, result)
 
     def test_several_appearances(self):
         result = locate_position(self.numbers2, 4)
-        self.assertEqual(result, 13)
+        self.assertEqual(13, result)
 
 
 if __name__ == '__main__':
