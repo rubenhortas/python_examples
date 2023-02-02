4d6e582506479db0c94b1267289d897ba19cfe4d
rubenhortas <rubenhortas@gmail.com>
2023-02-02 18:32:58 +0100

    Updated

--- data_structures_and_algorithms/rotating_lists/rotating_lists.md
+++
@@ -1,6 +0,0 @@
-You are given a list of numbers, obtained by rotating a sorted list an unknown number of times.
-Write a function to determine the minimum number of times the original **sorted list** was rotated to obtain the given list.
-Your function should have the worst-case complexity of **O(log(N))**.
-You can assume that **all the numbers in the list are unique**.
-
-Example: The list [5, 6, 9, 0, 2, 3, 4] was obtained by rotating the sorted list [0, 2, 3, 4, 5, 6, 9] three times.
--- data_structures_and_algorithms/rotating_lists/rotating_lists.py
+++ data_structures_and_algorithms/rotating_lists/rotating_lists.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+
+# You are given a list of numbers, obtained by rotating a sorted list an unknown number of times.
+# Write a function to determine the minimum number of times the original **sorted list** was rotated to obtain the given list.
+# Your function should have the worst-case complexity of **O(log(n))**.
+# You can assume that **all the numbers in the list are unique**.
+#Example: The list [5, 6, 9, 0, 2, 3, 4] was obtained by rotating the sorted list [0, 2, 3, 4, 5, 6, 9] three times.
+
 # Time Complexity: O(log(n))
 # Auxiliary Space: O(1)
 def calculate_rotations(lst):
