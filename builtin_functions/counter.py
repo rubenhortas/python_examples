f1b3cd0d635b94e0fcde890910b3de6a837745cd
rubenhortas <rubenhortas@gmail.com>
2023-02-12 14:22:15 +0100

    Supressed warning

--- builtin_functions/counter.py
+++ builtin_functions/counter.py
@@ -3,6 +3,7 @@
 # You can use Counter from the collections' library to get a dictionary with counts of all the unique elements in a list
 from collections import Counter
 
+# noinspection SpellCheckingInspection
 LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
 
 if __name__ == '__main__':
