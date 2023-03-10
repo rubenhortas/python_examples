7c5b85ed571d85265d04ff3e8e2ffd4f083c954d
rubenhortas <rubenhortas@gmail.com>
2023-03-10 21:15:21 +0100

    Added

---
+++ sysadmin/process_hunter.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+# This script looks for programs consuming CPU above a certain threshold and logs the results.
+
+from psutil import cpu_percent, process_iter, NoSuchProcess, AccessDenied, ZombieProcess
+from signal import signal, SIGINT
+from datetime import datetime
+
+CPU_THRESHOLD = 95  # Percentage, ex: 95%
+NUMBER_OF_PROCESSES = 3
+LOG_FILE = 'process_hunter.log'
+
+
+# noinspection PyUnusedLocal
+def exit_signal_handler(signal_, frame):
+    exit(0)
+
+
+def main():
+    while True:
+        if cpu_percent() >= CPU_THRESHOLD:
+            processes = []
+
+            for process in process_iter():
+                try:
+                    process_info = process.as_dict(attrs=['name', 'cpu_percent'])
+                    process_name = process_info['name'].lower()
+
+                    if 'idle' not in process_name and 'system' not in process_name:
+                        processes.append(process_info)
+                except (NoSuchProcess, AccessDenied, ZombieProcess):
+                    pass
+
+            sorted_processes = sorted(processes[0:NUMBER_OF_PROCESSES], key=lambda p: p['cpu_percent'], reverse=True)
+
+            write_log(sorted_processes)
+
+
+def write_log(processes):
+    log_line = f"{datetime.now().strftime('%Y/%m/%d %H:%M:%S')} "
+
+    for process in processes:
+        log_line = log_line + f"{process['name']}: {process['cpu_percent']}% "
+
+    try:
+        with open(LOG_FILE, 'a') as log:
+            log.write(log_line)
+    except (IOError, OSError, PermissionError):
+        pass
+
+
+if __name__ == '__main__':
+    signal(SIGINT, exit_signal_handler)
+    main()
