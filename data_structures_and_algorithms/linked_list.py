105e470803993b0e79c6aca887c14414f64adba2
rubenhortas <rubenhortas@gmail.com>
2023-03-23 21:38:44 +0100

    Fixed edge case: nodes = None

--- data_structures_and_algorithms/linked_list.py
+++ data_structures_and_algorithms/linked_list.py
@@ -5,7 +5,7 @@ class LinkedList:
     def __init__(self, nodes=None):
         self.head = None
 
-        if nodes is not None:
+        if nodes:
             node = Node(nodes.pop(0))
             self.head = node
 
