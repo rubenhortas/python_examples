1268d0404f888b87c227fcc975d9658f74d3f133
rubenhortas <rubenhortas@gmail.com>
2023-03-23 21:40:44 +0100

    Fixed edge case: nodes = None

--- data_structures_and_algorithms/doubly_linked_list.py
+++ data_structures_and_algorithms/doubly_linked_list.py
@@ -4,7 +4,7 @@ class DoublyLinkedList:
     def __init__(self, nodes=None):
         self.head = None
 
-        if nodes is not None:
+        if nodes:
             node = Node(nodes.pop(0))
             self.head = node
 
