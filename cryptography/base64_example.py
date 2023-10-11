429dfa508fb53f580423c6ea8e8b12f1afd41697
rubenhortas <rubenhortas@gmail.com>
2023-10-11 20:00:08 +0200

    Added comments.

--- cryptography/base64_example.py
+++ cryptography/base64_example.py
@@ -6,12 +6,15 @@ MESSAGE = "Hello world!"
 
 if __name__ == '__main__':
     print(f"message: {MESSAGE}")
+    # return: message: Hello world!
 
-    # Encode
+    # Encode to base64
     message_bytes = MESSAGE.encode()
     base64_message = base64.b64encode(message_bytes).decode()  # convert from bytes to string
     print(f"base64_message: {base64_message}")
+    # return: base64_message: SGVsbG8gd29ybGQh
 
-    # Decode
+    # Decode from base64
     decoded_message = base64.b64decode(base64_message).decode()  # convert from bytes to string
     print(f"decoded_message: {decoded_message}")
+    # return: decoded_message: Hello world!
