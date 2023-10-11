e0168f9c34d103bb5bb4aeb54ae16a5d1ae3ff79
rubenhortas <rubenhortas@gmail.com>
2023-10-11 19:11:11 +0200

    Refactored.

--- classes/data_classes/data_class.py
+++ classes/data_classes/data_class.py
@@ -2,12 +2,15 @@
 
 from dataclasses import dataclass
 
-# Since version 3.7, Python offers data classes.
-# Advantages:
-#    - requires a minimal amount of code
-#    - __eq__ is implemented
-#    - __repr__ is implemented (for print)
-#    - requires type hints (reduces the chances of bugs)
+"""
+ Since version 3.7, Python offers data classes.
+ 
+ Advantages:
+    - requires a minimal amount of code
+    - __eq__ is implemented
+    - __repr__ is implemented (for print)
+    - requires type hints (reduces the chances of bugs)
+"""
 if __name__ == '__main__':
 
     @dataclass
@@ -15,8 +18,14 @@ if __name__ == '__main__':
         name: str
         suit: str
 
-    fighter = Fighter("Ryu", "black")
+    fighter1 = Fighter("Ryu", "black")
+    fighter2 = Fighter("Ryu", "black")
 
-    print(fighter == fighter)  # True
-    print(fighter.name)  # Ryu
-    print(fighter)  # Fighter(name='Ryu', suit='black')
+    print(fighter1 == fighter2)
+    # return: True
+
+    print(fighter1.name)
+    # return: Ryu
+
+    print(fighter1)
+    # return: Fighter(name='Ryu', suit='black')
