d8f598a735a99a109dec0ddfff6005f58ceef4e3
rubenhortas <rubenhortas@gmail.com>
2023-10-16 20:59:09 +0200

    Added comments.

--- data_structures_and_algorithms/queue_.py
+++ data_structures_and_algorithms/queue_.py
@@ -11,7 +11,13 @@ if __name__ == '__main__':
     queue.append("Third")
 
     print(queue)
+    # return: deque(['First', 'Second', 'Third'])
 
     for i in list(queue):
         queue.popleft()  # Since queues are FIFO, the first element who got into the queue should be the first to get out
         print(queue)
+
+    # return:
+    #   deque(['Second', 'Third'])
+    #   deque(['Third'])
+    #   deque([])
