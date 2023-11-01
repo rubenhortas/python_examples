6962114dace0bc0aefcf3402e602cee66856aede
rubenhortas <rubenhortas@gmail.com>
2023-11-01 10:39:33 +0100

    Added comments.

--- performance/object_memory_measure.py
+++ performance/object_memory_measure.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     numbers_range = range(100)
     numbers_list = [i for i in range(100)]
 
-    # The range function returns a class that behaves like a list
-    # A range is much more memory efficient than a list
-    print(f"Size of range: {sys.getsizeof(numbers_range)} bytes")  # 48 bytes
-    print(f"Size of list: {sys.getsizeof(numbers_list)} bytes")
+    # The range function returns a class that behaves like a list.
+    # A range is much more memory efficient than a list.
+    print(f"Size of range: {sys.getsizeof(numbers_range)} bytes")  # Size of range: 48 bytes
+    print(f"Size of list: {sys.getsizeof(numbers_list)} bytes")  # Size of list: 920 bytes
