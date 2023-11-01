8066124bf8a097098a8bdd131b7c87bc28eb6774
rubenhortas <rubenhortas@gmail.com>
2023-11-01 10:29:47 +0100

    Fixed bug.

--- data_structures_and_algorithms/fibonacci/fibonacci_bottom_up.py
+++ data_structures_and_algorithms/fibonacci/fibonacci_bottom_up.py
@@ -8,13 +8,16 @@ This approach consists of build the solution for one case off the previous case 
 # Time complexity: O(n)
 # Auxiliary space: O(1)
 def fibonacci(n: int) -> int:
+    if n <= 1:
+        return n
+
     # We don't need to store the computed values into an array.
     # We only need the last two values.
     result = 0
     nm2 = 0  # n minus 2, n - 2
     nm1 = 1  # n minus 1, n - 1
 
-    for i in range(1, n + 1):
+    for _ in range(2, n + 1):
         result = nm1 + nm2
 
         nm2 = nm1
