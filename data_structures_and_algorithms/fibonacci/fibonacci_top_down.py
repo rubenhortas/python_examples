bdc0cfc2ad1abefb4c5ad3306c9d51be682d1b90
rubenhortas <rubenhortas@gmail.com>
2023-11-02 19:52:24 +0100

    Added closure.

--- data_structures_and_algorithms/fibonacci/fibonacci_top_down.py
+++ data_structures_and_algorithms/fibonacci/fibonacci_top_down.py
@@ -12,14 +12,14 @@ This approach consists of dividing the problem case N into subproblems.
 def fibonacci(n: int) -> int:
     values = [-1] * (n + 1)
 
-    return _fibonacci(n, values)
+    # noinspection PyShadowingNames
+    def _fibonacci(n: int) -> int:
+        if n <= 1:
+            values[n] = n  # or return n
 
+        if values[n] == -1:
+            values[n] = _fibonacci(n - 1) + _fibonacci(n - 2)
 
-def _fibonacci(n: int, values: list) -> int:
-    if n <= 1:
-        values[n] = n  # or return n
+        return values[n]
 
-    if values[n] == -1:
-        values[n] = _fibonacci(n - 1, values) + _fibonacci(n - 2, values)
-
-    return values[n]
+    return _fibonacci(n)
