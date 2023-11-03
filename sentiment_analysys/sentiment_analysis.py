f0d78fa9fbb11a36f558893be1edbd6b324c763f
rubenhortas <rubenhortas@gmail.com>
2023-11-03 14:14:08 +0100

    Refactored.

--- sentiment_analysys/sentiment_analysis.py
+++ sentiment_analysys/sentiment_analysis.py
@@ -5,18 +5,18 @@ ENCODING = 'UTF-8'
 DECIMAL_NUMBERS = 2
 
 if __name__ == '__main__':
-    f_input = open('input.txt', 'r', encoding=ENCODING)
-    f_results = open('results.txt', 'w+', encoding=ENCODING)
-    f_results.write("line,polarity,subjectivity\n")
+    input_ = open('input.txt', 'r', encoding=ENCODING)
+    results = open('results.txt', 'w+', encoding=ENCODING)
+    results.write("line,polarity,subjectivity\n")
 
-    for line in f_input.read().split('\n'):
+    for line in input_.read().split('\n'):
         try:
             analysis = TextBlob(line)
 
             line_polarity = round(analysis.polarity, DECIMAL_NUMBERS)
             line_subjectivity = round(analysis.subjectivity, DECIMAL_NUMBERS)
 
-            f_results.write(f'{line},{line_polarity},{line_subjectivity}\n')
+            results.write(f'{line},{line_polarity},{line_subjectivity}\n')
         except TranslatorError as translator_exception:
             pass
         except Exception as e:
