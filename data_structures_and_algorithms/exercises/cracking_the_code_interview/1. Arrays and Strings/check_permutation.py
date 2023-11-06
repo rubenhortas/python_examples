4dd1a70ca64f3e03037e2d7539581ff9a0b82eb7
rubenhortas <rubenhortas@gmail.com>
2023-11-06 23:30:07 +0100

    Added

---
+++ data_structures_and_algorithms/exercises/cracking_the_code_interview/1. Arrays and Strings/check_permutation.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+# Given two strings, write a method to decide if one is a permutation of the other.
+
+# We will assume the following:
+#   * Permutation is case-sensitive
+#   * Whitespaces are significant
+#   * ASCII encoding
+
+STR1 = 'check permutations'
+STR2 = 'permutations check'
+
+
+def _is_permutation_sorting(str1: str, str2: str) -> bool:
+    # Time complexity: O(nlog(n))
+    # Auxiliary space: O(1)
+    if len(str1) != len(str2):
+        return False
+
+    return sorted(str1) == sorted(str2)
+
+
+def _is_permutation(str1: str, str2: str) -> bool:
+    # Time complexity: O(n)
+    # Auxiliary space: O(1)
+    counter = [0] * 128
+
+    if len(str1) != len(str2):
+        return False
+
+    for i in range(len(str1)):
+        char_position = ord(str1[i])
+        counter[char_position] = counter[char_position] + 1
+
+    for i in range(len(str2)):
+        char_position = ord(str2[i])
+        counter[char_position] = counter[char_position] - 1
+
+        if counter[ord(str2[i])] < 0:
+            return False
+
+    return True
+
+
+if __name__ == '__main__':
+    print(_is_permutation(STR1, STR2))
+    print(_is_permutation_sorting(STR1, STR2))
