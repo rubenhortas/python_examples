7cac4e4ab33925f2f3bbcb041fd8f55092b5a9c1
rubenhortas <rubenhortas@gmail.com>
2024-01-23 18:53:00 +0100

    Added.

---
+++ misc/data_loader_tqdm.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from tqdm import tqdm
+from time import sleep
+
+if __name__ == '__main__':
+
+    data = list(range(1000))
+
+    for i in tqdm(data):
+        sleep(0.01)
