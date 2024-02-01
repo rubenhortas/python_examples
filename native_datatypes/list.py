7bca743378db09c9040746593dc1bdb02ffd9f96
rubenhortas <rubenhortas@gmail.com>
2024-02-01 17:03:31 +0100

    Fixed comments

--- native_datatypes/list.py
+++ native_datatypes/list.py
@@ -133,12 +133,14 @@ if __name__ == '__main__':
     # return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] backwards is [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 
     join()
-    # return: Join ['a', 'b', 'c', 'dct', 'e', 'f'] and ['dct', 'e', 'f']
-    #         Join with +: ['a', 'b', 'c', 'dct', 'e', 'f']
-    #         Join with extend: ['a', 'b', 'c', 'dct', 'e', 'f']
+    # return:
+    # Join ['a', 'b', 'c', 'dct', 'e', 'f'] and ['dct', 'e', 'f']
+    # Join with +: ['a', 'b', 'c', 'dct', 'e', 'f']
+    # Join with extend: ['a', 'b', 'c', 'dct', 'e', 'f']
 
     slice()
-    # return: {lst[0:5]}: [0, 1, 2, 3, 4]
-    #         {lst[-5::1]}: [5, 6, 7, 8, 9]
-    #         {lst[::2]}: [0, 2, 4, 6, 8]
-    #         new_list = [0, 1, 8, 9]
+    # return:
+    # {lst[0:5]}: [0, 1, 2, 3, 4]
+    # {lst[-5::1]}: [5, 6, 7, 8, 9]
+    # {lst[::2]}: [0, 2, 4, 6, 8]
+    # new_list = [0, 1, 8, 9]
