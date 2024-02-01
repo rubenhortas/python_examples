d994a04d5af4ff4cceea87951d5fa42a09c12ea4
rubenhortas <rubenhortas@gmail.com>
2024-02-01 17:02:16 +0100

    Fixed comment

--- data_structures_and_algorithms/queue_.py
+++ data_structures_and_algorithms/queue_.py
@@ -18,6 +18,6 @@ if __name__ == '__main__':
         print(queue)
 
     # return:
-    #   deque(['Second', 'Third'])
-    #   deque(['Third'])
-    #   deque([])
+    # deque(['Second', 'Third'])
+    # deque(['Third'])
+    # deque([])
