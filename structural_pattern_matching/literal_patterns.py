bb143716727a3dc0e951d87c2cf569e0f3b19b80
rubenhortas <rubenhortas@gmail.com>
2024-03-28 00:47:48 +0100

    Deleted documentation

--- structural_pattern_matching/literal_patterns.py
+++ structural_pattern_matching/literal_patterns.py
@@ -1,32 +1,12 @@
 #!/usr/bin/env python3
 
-"""
-https://peps.python.org/pep-0622/
-Pattern matching statement to Python, inspired by similar syntax found in Scala, Erlang, and other languages.
-
-A match statement compares a value (the subject) to several different shapes (the patterns) until a shape fits.
-
-Syntactically, a match statement contains:
-    - a subject expression
-    - one or more case clauses
-
-Each case clause specifies:
-    - a pattern (the overall shape to be matched)
-    - an optional “guard” (a condition to be checked if the pattern matches)
-    - a code block to be executed if the case clause is selected
-"""
-
 
 def _get_http_status(code: int) -> str:
-    # The match statement tries to match a single subject to each of the patterns in its case clauses.
-    # At the first successful match to a pattern in a case clause the variables in the pattern are assigned, and a corresponding block is executed.
-    # Each case clause can also specify an optional boolean condition, known as a guard.
-    match code:  # The subject
-        # The patterns
-        # Each pattern describes the type and structure of the accepted values as well as the variables where to capture its contents.
-        case None:  # Literal pattern: number, string, 'True', 'False', None
+    # Literal pattern: number, string, 'True', 'False', None
+    match code:
+        case None:
             return 'None'
-        case True | False:  # Literal pattern: number, string, 'True', 'False', None
+        case True | False:
             return '{status}'
         case 400:
             return 'https://http.cat/status/400'
