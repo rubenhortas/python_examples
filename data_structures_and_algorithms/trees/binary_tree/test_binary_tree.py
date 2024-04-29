55fef82d9fe3b08e360f3967221c3d8cadaf4238
rubenhortas <rubenhortas@gmail.com>
2024-04-29 21:27:54 +0200

    Fixed method name

--- data_structures_and_algorithms/trees/binary_tree/test_binary_tree.py
+++ data_structures_and_algorithms/trees/binary_tree/test_binary_tree.py
@@ -16,7 +16,7 @@ class Test(unittest.TestCase):
         self.inorder_traversal_expected_values = ['4', '2', '5', '1', '6', '3', '7']
         self.preorder_traversal_expected_values = ['1', '2', '4', '5', '3', '6', '7']
         self.postorder_traversal_expected_values = ['4', '5', '2', '6', '7', '3', '1']
-        self.levelorder_traversal_expected_values = ['1', '2', '3', '4', '5', '6', '7']
+        self.level_order_traversal_expected_values = ['1', '2', '3', '4', '5', '6', '7']
 
     def test_print_inorder_traversal(self):
         self.assertEqual(self.binary_tree.get_inorder_traversal(), self.inorder_traversal_expected_values)
@@ -27,5 +27,5 @@ class Test(unittest.TestCase):
     def test_print_postorder_traversal(self):
         self.assertEqual(self.binary_tree.get_postorder_traversal(), self.postorder_traversal_expected_values)
 
-    def test_print_levelorder_traversal(self):
-        self.assertEqual(self.binary_tree.get_level_order_traversal(), self.levelorder_traversal_expected_values)
+    def test_print_level_order_traversal(self):
+        self.assertEqual(self.binary_tree.get_level_order_traversal(), self.level_order_traversal_expected_values)
