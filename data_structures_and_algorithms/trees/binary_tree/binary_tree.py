1837d5e4b88bc285807053ea71ef18628d66f0bd
rubenhortas <rubenhortas@gmail.com>
2024-04-29 21:26:39 +0200

    Refactored

--- data_structures_and_algorithms/trees/binary_tree/binary_tree.py
+++ data_structures_and_algorithms/trees/binary_tree/binary_tree.py
@@ -13,67 +13,90 @@ class Node:
         return str(self.value)
 
 
-def print_inorder_traversal(node: Node) -> None:
-    """
-    1. Traverse the left subtree
-    2. Visit the root
-    3. Traverse the right subtree
+class BinaryTree:
+    def __init__(self, node: Node):
+        self.root = node
 
-    Time complexity: O(n)
-    Auxiliary space: O(1)
-    """
-    if node:
-        print_inorder_traversal(node.left)
-        print(node, end=' ')
-        print_inorder_traversal(node.right)
+    def get_inorder_traversal(self) -> list:
+        """
+        1. Traverse the left subtree
+        2. Visit the root
+        3. Traverse the right subtree
 
+        Time complexity: O(n)
+        Auxiliary space: O(1)
+        """
 
-def print_preorder_traversal(node: Node) -> None:
-    """
-    1. Visit the root
-    2. Traverse the left subtree
-    3. Traverse the right subtree
-
-    Time complexity: O(n)
-    Auxiliary space: O(1)
-    """
-    if node:
-        print(node, end=' ')
-        print_preorder_traversal(node.left)
-        print_preorder_traversal(node.right)
-
-
-def print_postorder_traversal(node: Node) -> None:
-    """
-    1. Traverse the left subtree
-    2. Traverse the right subtree
-    3. Visit the root
-
-    Time complexity: O(n)
-    Auxiliary space: O(1)
-    """
-    if node:
-        print_postorder_traversal(node.left)
-        print_postorder_traversal(node.right)
-        print(node, end=' ')
-
-
-def print_level_order_traversal(root: Node) -> None:
-    """
-    Level order tree traversal algorithm is a breadth-first tree traversal algorithm.
-    It means that while traversing a tree, we first traverse all the elements at the current level before moving to the next level.
-
-    Time complexity: O(n)
-    Auxiliary space: O(1)
-    """
-    if root:
-        queue = Queue()
-        queue.put(root)
-
-        while not queue.empty():
-            node = queue.get()
-
+        def traverse_inorder(node: Node, values: list) -> None:
             if node:
-                print(node, end=' ')
-                queue.put(node.left)
-                queue.put(node.right)
+                traverse_inorder(node.left, values)
+                values.append(str(node))
+                traverse_inorder(node.right, values)
+
+        values = []
+        traverse_inorder(self.root, values)
+        return values
+
+    def get_preorder_traversal(self) -> list:
+        """
+        1. Visit the root
+        2. Traverse the left subtree
+        3. Traverse the right subtree
+
+        Time complexity: O(n)
+        Auxiliary space: O(1)
+        """
+
+        def traverse_preorder(node: Node, values: list) -> None:
+            if node:
+                values.append(str(node))
+                traverse_preorder(node.left, values)
+                traverse_preorder(node.right, values)
+
+        values = []
+        traverse_preorder(self.root, values)
+        return values
+
+    def get_postorder_traversal(self) -> list:
+        """
+        1. Traverse the left subtree
+        2. Traverse the right subtree
+        3. Visit the root
+
+        Time complexity: O(n)
+        Auxiliary space: O(1)
+        """
+
+        def traverse_postorder(node: Node, values: list) -> None:
+            if node:
+                traverse_postorder(node.left, values)
+                traverse_postorder(node.right, values)
+                values.append(str(node))
+
+        values = []
+        traverse_postorder(self.root, values)
+        return values
+
+    def get_level_order_traversal(self) -> list:
+        """
+        Level order tree traversal algorithm is a breadth-first tree traversal algorithm.
+        It means that while traversing a tree, we first traverse all the elements at the current level before moving to the next level.
+
+        Time complexity: O(n)
+        Auxiliary space: O(1)
+        """
+        values = []
+
+        if self.root:
+            queue = Queue()
+            queue.put(self.root)
+
+            while not queue.empty():
+                node = queue.get()
+
+                if node:
+                    values.append(str(node))
+                    queue.put(node.left)
+                    queue.put(node.right)
+
+        return values
--- data_structures_and_algorithms/trees/binary_tree/test_binary_tree.py
+++ data_structures_and_algorithms/trees/binary_tree/test_binary_tree.py
@@ -12,24 +12,20 @@ class Test(unittest.TestCase):
         self.root.left.right = Node(5)
         self.root.right.left = Node(6)
         self.root.right.right = Node(7)
+        self.binary_tree = BinaryTree(self.root)
+        self.inorder_traversal_expected_values = ['4', '2', '5', '1', '6', '3', '7']
+        self.preorder_traversal_expected_values = ['1', '2', '4', '5', '3', '6', '7']
+        self.postorder_traversal_expected_values = ['4', '5', '2', '6', '7', '3', '1']
+        self.levelorder_traversal_expected_values = ['1', '2', '3', '4', '5', '6', '7']
 
-    def test_print_inorder(self):
-        print_inorder_traversal(self.root)
-        print()
-        # Expected: 4 2 5 1 6 3 7
+    def test_print_inorder_traversal(self):
+        self.assertEqual(self.binary_tree.get_inorder_traversal(), self.inorder_traversal_expected_values)
 
-    def test_print_preorder(self):
-        print_preorder_traversal(self.root)
-        print()
-        # Expected: 1 2 4 5 3 6 7
+    def test_print_preorder_traversal(self):
+        self.assertEquals(self.binary_tree.get_preorder_traversal(), self.preorder_traversal_expected_values)
 
-    def test_print_postorder(self):
-        print_postorder_traversal(self.root)
-        print()
-        # Expected: 4 5 2 6 7 3 1
+    def test_print_postorder_traversal(self):
+        self.assertEqual(self.binary_tree.get_postorder_traversal(), self.postorder_traversal_expected_values)
 
-    def test_print_levelorder(self):
-        print_level_order_traversal(self.root)
-        print()
-        # Expected:
-
+    def test_print_levelorder_traversal(self):
+        self.assertEqual(self.binary_tree.get_level_order_traversal(), self.levelorder_traversal_expected_values)
