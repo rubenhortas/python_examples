013afe89e9c81009d06b2490b4af782622770d7e
rubenhortas <rubenhortas@gmail.com>
2024-12-23 11:37:33 +0100

    Added comments

--- data_structures_and_algorithms/algorithms/backtracking/the_sums/sums.py
+++ data_structures_and_algorithms/algorithms/backtracking/the_sums/sums.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
 def get_sums(numbers: list, target: int) -> list:
+    """
+    Finds all combinations of numbers in a list that add up to the target value.
+    If there are no combinations, return an empty list.
+    :param numbers: [1, 5, 3, 2]
+    :param target: 6
+    :return: [[1, 5],  [1, 3, 2]]
+    """
+
     def get_sum(combination: list, start: int, target: int) -> list | None:
         if target == 0:  # Solution found
             solutions.append(combination[:])  # Shallow copy
