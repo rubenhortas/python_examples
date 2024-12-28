63f37a24a30ddd3a8a8dbc5448a5540bf20fc442
rubenhortas <rubenhortas@gmail.com>
2024-12-28 13:25:35 +0100

    Refactored shunting yard tests

--- data_structures_and_algorithms/algorithms/shunting_yard/test_shunting_yard.py
+++ data_structures_and_algorithms/algorithms/shunting_yard/test_shunting_yard.py
@@ -5,24 +5,19 @@ from data_structures_and_algorithms.algorithms.shunting_yard.shunting_yard impor
 
 
 class Test(unittest.TestCase):
-    def setUp(self):
-        self.infix_expressions = [
+    def test_shunting_yard(self):
+        infix_expressions = [
             ('1 + 2 * 3 + (4 + 5 * 6)', '1 2 3 * + 4 5 6 * + +'),
             ('11 + 22 * 33 + (44 + 55 * 66)', '11 22 33 * + 44 55 66 * + +')
         ]
 
-        self.rpn_expressions = [
-            ('1 2 3 * + 4 5 6 * + +', '((1+(2*3))+(4+(5*6)))')
-        ]
-
-    def test_shunting_yard(self):
-        for expression, expected_result in self.infix_expressions:
+        for expression, expected_result in infix_expressions:
             self.assertEquals(expected_result, shunting_yard(expression))
 
     def test_rpn_to_infix(self):
-        for expression, expected_result in self.rpn_expressions:
+        rpn_expressions = [
+            ('1 2 3 * + 4 5 6 * + +', '((1+(2*3))+(4+(5*6)))')
+        ]
+
+        for expression, expected_result in rpn_expressions:
             self.assertEquals(expected_result, rpn_to_infix(expression))
-
-
-if __name__ == '__main__':
-    unittest.main()
