4478d6a6b17a3f3564dd3e7f16f128694326cf22
rubenhortas <rubenhortas@gmail.com>
2024-12-28 13:39:04 +0100

    Refactored avl tests

--- data_structures_and_algorithms/data_structures/trees/avl_tree/test_avl.py
+++ data_structures_and_algorithms/data_structures/trees/avl_tree/test_avl.py
@@ -4,16 +4,15 @@ from avl_tree import AvlTree
 
 
 class Test(unittest.TestCase):
-    def setUp(self):
-        self.avl = AvlTree()
-        self.avl.insert_value(1)
-        self.avl.insert_value(2)
-        self.avl.insert_value(3)
-        self.avl.insert_value(4)
-        self.avl.insert_value(5)
-
     def test_avl(self):
-        self.assertEqual([2, 1, 4, 3, 5], self.avl.preorder())
+        avl = AvlTree()
+        avl.insert_value(1)
+        avl.insert_value(2)
+        avl.insert_value(3)
+        avl.insert_value(4)
+        avl.insert_value(5)
+        
+        self.assertEqual([2, 1, 4, 3, 5], avl.preorder())
 
-        self.avl.delete_value(2)
-        self.assertEqual([3, 1, 4, 5], self.avl.preorder())
+        avl.delete_value(2)
+        self.assertEqual([3, 1, 4, 5], avl.preorder())
