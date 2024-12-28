e953464ae04f7b184e6a42cb94dbfa41645fce7e
rubenhortas <rubenhortas@gmail.com>
2024-12-28 13:39:54 +0100

    Refactored bst tests

--- data_structures_and_algorithms/data_structures/trees/binary_search_tree/test_binary_search_tree.py
+++ data_structures_and_algorithms/data_structures/trees/binary_search_tree/test_binary_search_tree.py
@@ -4,16 +4,15 @@ from binary_search_tree import BinarySearchTree
 
 
 class Test(unittest.TestCase):
-    def setUp(self):
-        self.bst = BinarySearchTree()
-        self.bst.insert(7)
-        self.bst.insert(5)
-        self.bst.insert(6)
-        self.bst.insert(12)
-        self.bst.insert(4)
-        self.bst.insert(6)
-        self.bst.insert(5)
-        self.bst.insert(7)
-
     def test_insert(self):
-        self.assertEqual(['4 (1)', '5 (2)', '6 (2)', '7 (2)', '12 (1)'], self.bst.get_inorder_traversal())
+        bst = BinarySearchTree()
+        bst.insert(7)
+        bst.insert(5)
+        bst.insert(6)
+        bst.insert(12)
+        bst.insert(4)
+        bst.insert(6)
+        bst.insert(5)
+        bst.insert(7)
+
+        self.assertEqual(['4 (1)', '5 (2)', '6 (2)', '7 (2)', '12 (1)'], bst.get_inorder_traversal())
