8ae9b1e212ae551372478e07c8654352849e1335
rubenhortas <rubenhortas@gmail.com>
2024-12-28 13:47:14 +0100

    Refactored binary search tests

--- data_structures_and_algorithms/algorithms/binary_search/test.py
+++ data_structures_and_algorithms/algorithms/binary_search/test.py
@@ -11,19 +11,19 @@ class Test(unittest.TestCase):
         self.numbers = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
         self.numbers2 = [9, 9, 9, 8, 8, 8, 7, 7, 7, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1, 0, 0, 0]
 
-    def test_in_the_middle(self):
+    def test_get_middle_position(self):
         result = get_position(self.numbers, 4)
         self.assertEqual(5, result)
 
-    def test_is_first_element(self):
+    def test_get_first_element(self):
         result = get_position(self.numbers, 9)
         self.assertEqual(0, result)
 
-    def test_is_last_element(self):
+    def test_get_last_element(self):
         result = get_position(self.numbers, 0)
         self.assertEqual(9, result)
 
-    def test_is_the_only_number(self):
+    def test_get_the_only_number(self):
         result = get_position([1], 1)
         self.assertEqual(0, result)
 
@@ -42,7 +42,3 @@ class Test(unittest.TestCase):
     def test_several_appearances(self):
         result = get_position(self.numbers2, 4)
         self.assertEqual(13, result)
-
-
-if __name__ == '__main__':
-    unittest.main()  # Run all tests
