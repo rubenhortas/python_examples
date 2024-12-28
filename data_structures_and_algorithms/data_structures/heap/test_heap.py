958ec980584b4b10c68e06020d629ab74029c8c5
rubenhortas <rubenhortas@gmail.com>
2024-12-28 13:27:20 +0100

    Refactored heap tests

--- data_structures_and_algorithms/data_structures/heap/test_heap.py
+++ data_structures_and_algorithms/data_structures/heap/test_heap.py
@@ -1,33 +1,32 @@
 import unittest
 
-from min_heap import MinHeap
 from max_heap import MaxHeap
+from min_heap import MinHeap
 
 
 class Test(unittest.TestCase):
-    def setUp(self):
-        self.min_heap = MinHeap(10)
-        self.min_heap.insert(10)
-        self.min_heap.insert(5)
-        self.min_heap.insert(15)
-        self.min_heap.insert(4)
-        self.min_heap_expected_min = 4
-        self.min_heap_expected_second_min = 5
-        
-        self.max_heap = MaxHeap(10)
-        self.max_heap.insert(10)
-        self.max_heap.insert(5)
-        self.max_heap.insert(15)
-        self.max_heap.insert(4)
-        self.max_heap_expected_max = 15
-        self.max_heap_expected_second_max = 10
-
     def test_min_heap(self):
-        self.assertEqual(self.min_heap_expected_min, self.min_heap.get_min())
-        self.assertEqual(self.min_heap_expected_min, self.min_heap.extract_min())
-        self.assertEqual(self.min_heap_expected_second_min, self.min_heap.get_min())
-        
+        min_heap = MinHeap(10)
+        min_heap.insert(10)
+        min_heap.insert(5)
+        min_heap.insert(15)
+        min_heap.insert(4)
+        min_heap_expected_min = 4
+        min_heap_expected_second_min = 5
+
+        self.assertEqual(min_heap_expected_min, min_heap.get_min())
+        self.assertEqual(min_heap_expected_min, min_heap.extract_min())
+        self.assertEqual(min_heap_expected_second_min, min_heap.get_min())
+
     def test_max_heap(self):
-        self.assertEqual(self.max_heap_expected_max, self.max_heap.get_max())
-        self.assertEqual(self.max_heap_expected_max, self.max_heap.extract_max())
-        self.assertEqual(self.max_heap_expected_second_max, self.max_heap.get_max())
+        max_heap = MaxHeap(10)
+        max_heap.insert(10)
+        max_heap.insert(5)
+        max_heap.insert(15)
+        max_heap.insert(4)
+        max_heap_expected_max = 15
+        max_heap_expected_second_max = 10
+
+        self.assertEqual(max_heap_expected_max, max_heap.get_max())
+        self.assertEqual(max_heap_expected_max, max_heap.extract_max())
+        self.assertEqual(max_heap_expected_second_max, max_heap.get_max())
