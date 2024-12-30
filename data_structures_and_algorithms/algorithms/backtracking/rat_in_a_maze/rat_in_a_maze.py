709aea2f3716c69acae0054a6b283a0389c9c30a
rubenhortas <rubenhortas@gmail.com>
2024-12-30 18:15:00 +0100

    Improved comments

--- data_structures_and_algorithms/algorithms/backtracking/rat_in_a_maze/rat_in_a_maze.py
+++ data_structures_and_algorithms/algorithms/backtracking/rat_in_a_maze/rat_in_a_maze.py
@@ -3,6 +3,22 @@ from data_structures_and_algorithms.algorithms.backtracking.rat_in_a_maze.rat im
 
 
 def get_paths(maze: Maze) -> list | None:
+    """
+    Consider a rat placed at (0, 0) in a square matrix of order N*N.
+    Returns all possible paths (in increasing lexicographically order) that the rat can take
+    to get from (0,0) to (N-1, N-1).
+    
+    The directions in which the rat can move are 'U' (Up), 'D' (Down), 'L' (Left), 'R' (Right).
+
+    The value 0 in a cell of the matrix represents that it is blocked and rat cannot move to it.
+    The value 1 in a cell of the matrix represents that rat can be travel through it.
+
+    In a path, no cell can be visited more than once.
+
+    :param maze: [[1, 0, 0, 0],[1, 1, 0, 1],[0, 1, 0, 0],[1, 1, 1, 1]]
+    :return: ['DRDDRR']
+    """
+
     def get_path(row: int, column: int, position: int, path: str) -> None:
         if row == maze.last_row and column == maze.last_column:
             paths.append(path)
--- data_structures_and_algorithms/algorithms/backtracking/rat_in_a_maze/test_rat_in_a_maze.py
+++ data_structures_and_algorithms/algorithms/backtracking/rat_in_a_maze/test_rat_in_a_maze.py
@@ -1,12 +1,12 @@
-# Consider a rat placed at (0, 0) in a square matrix of order N * N.
-# It has to reach the destination at (N – 1, N – 1).
-# Find all possible paths that the rat can take to reach from source to destination.
-# The directions in which the rat can move are ‘U'(up), ‘D'(down), ‘L’ (left), ‘R’ (right).
-# Value 0 at a cell in the matrix represents that it is blocked and rat cannot move to it
-# while value 1 at a cell in the matrix represents that rat can be travel through it.
+# Consider a rat located at (0, 0) in a square matrix of order N * N.
+# It must reach the destination at (N – 1, N – 1).
+# Find all possible paths the rat can take to reach from the source to the destination.
+# The directions in which the rat can move are 'U' (Up), 'D' (Down), 'L' (Left), 'R' (Right).
+# The value 0 in a cell of the matrix represents that it is blocked and the rat cannot move to it
+# while the value 1 in a cell of the matrix represents that the rat can be travel through it.
 # Return the list of paths in lexicographically increasing order.
-# Note: In a path, no cell can be visited more than one time. If the source cell is 0,
-# the rat cannot move to any other cell.
+# Note: In a path, no cell can be visited more than once.
+# If the source cell is 0, the rat cannot move to any other cell.
 import unittest
 
 from data_structures_and_algorithms.algorithms.backtracking.rat_in_a_maze.maze import Maze
