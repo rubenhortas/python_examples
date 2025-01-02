54d3250070b19d1a22b703d484b419b59f683f47
rubenhortas <rubenhortas@gmail.com>
2025-01-02 19:50:26 +0100

    Added merge sort

---
+++ data_structures_and_algorithms/algorithms/sort/merge_sort/merge_sort.py
@@ -0,0 +1,36 @@
+def sort(lst: list) -> list:
+    # Time complexity: O(n*log(n))
+    # Auxiliary space: O(n)
+
+    if len(lst) <= 1:
+        return lst
+
+    mid = len(lst) // 2
+    left_half = lst[:mid]
+    right_half = lst[mid:]
+
+    left_sorted = sort(left_half)
+    right_sorted = sort(right_half)
+
+    result = _merge(left_sorted, right_sorted)
+
+    return result
+
+
+def _merge(left, right):
+    result = []
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+
+    result.extend(left[left_index:])
+    result.extend(right[right_index:])
+
+    return result
