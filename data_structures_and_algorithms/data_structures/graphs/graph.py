21e4d69e8fd3f3a0bfd765b6df3341ecc1ddb478
rubenhortas <rubenhortas@gmail.com>
2025-01-08 20:41:08 +0100

    Improved graphs

--- data_structures_and_algorithms/data_structures/graphs/graph.py
+++ data_structures_and_algorithms/data_structures/graphs/graph.py
@@ -1,18 +1,37 @@
 class Graph:
-    """
-    Undirected and unweighted graph.
-    """
-
-    def __init__(self, num_nodes: int, edges: list):
+    def __init__(self, num_nodes: int, edges: list, directed: bool = False):
         self.num_nodes = num_nodes
-        self.data = [[] for _ in range(num_nodes)]
+        self.directed = directed
+        self.weighted = len(edges) > 0 and len(edges[0]) == 3
+        self.nodes = [[] for _ in range(num_nodes)]
+        self.weights = [[] for _ in range(num_nodes)]
 
-        for node1, node2 in edges:
-            self.data[node1].append(node2)
-            self.data[node2].append(node1)
+        for edge in edges:  # edge =  (node1, node2)|(node1, node2, weight)
+            self.nodes[edge[0]].append(edge[1])
+
+            if self.weighted:
+                self.weights[edge[0]].append(edge[2])
+
+            if not directed:
+                self.nodes[edge[1]].append(edge[0])
+
+                if self.weighted:
+                    self.weights[edge[1]].append(edge[2])
 
     def __repr__(self):
-        return '\n'.join([f"{node}: {neighbors}" for node, neighbors in enumerate(self.data)])
+        result = ''
+
+        if self.weighted:
+            print('# node1: [(node2, weight), ..., (node_n, weight)]')
+
+            for i, (nodes, weights) in enumerate(zip(self.nodes, self.weights)):
+                result += f"{i}: {list(zip(nodes, weights))}\n"
+        else:
+            print('# node1: [node2, ..., node_n]')
+
+            for i, nodes in enumerate(self.nodes):
+                result += f"{i}: {nodes}\n"
+        return result
 
     def __str__(self):
         return self.__repr__()
--- data_structures_and_algorithms/data_structures/graphs/test_graphs.py
+++ data_structures_and_algorithms/data_structures/graphs/test_graphs.py
@@ -6,16 +6,32 @@ from data_structures_and_algorithms.data_structures.graphs.graph import Graph
 
 class Test(unittest.TestCase):
     def setUp(self):
-        self.num_nodes = 5
-        self.edges = [(0, 1), (0, 4), (1, 2), (1, 3), (1, 4), (2, 3), (3, 4)]
+        self.undirected_unweighted_graph_nodes = 5
+        self.undirected_unweighted_graph_edges = [(0, 1), (0, 4), (1, 2), (1, 3), (1, 4), (2, 3), (3, 4)]
 
-    def test_graph(self):
-        graph = Graph(self.num_nodes, self.edges)
+    def test_undirected_unweighted_graph(self):
+        graph = Graph(self.undirected_unweighted_graph_nodes, self.undirected_unweighted_graph_edges)
 
-        self.assertEqual([[1, 4], [0, 2, 3, 4], [1, 3], [1, 2, 4], [0, 1, 3]], graph.data)
+        self.assertEqual([[1, 4], [0, 2, 3, 4], [1, 3], [1, 2, 4], [0, 1, 3]], graph.nodes)
+
+    def test_undirected_weighted_graph(self):
+        num_nodes = 9
+        edges = [(0, 1, 3), (0, 3, 2), (0, 8, 4), (1, 7, 4), (2, 7, 2), (2, 3, 6), (2, 5, 1), (3, 4, 1), (4, 8, 8),
+                 (5, 6, 8)]  # (node1, node2, weight)
+        graph = Graph(num_nodes, edges)
+
+        self.assertEqual([[1, 3, 8], [0, 7], [7, 3, 5], [0, 2, 4], [3, 8], [2, 6], [5], [1, 2], [0, 4]],
+                         graph.nodes)
+
+    def test_directed_unweighted_graph(self):
+        num_nodes = 5
+        edges = [(0, 1), (1, 2), (2, 3), (2, 4), (4, 2), (3, 0)]
+        graph = Graph(num_nodes, edges, directed=True)
+
+        self.assertEqual([[1], [2], [3, 4], [0], [2]], graph.nodes)
 
     def test_adjacency_matrix(self):
-        matrix = AdjacencyMatrix(self.num_nodes, self.edges)
+        matrix = AdjacencyMatrix(self.undirected_unweighted_graph_nodes, self.undirected_unweighted_graph_edges)
 
         self.assertEqual([[0, 1, 0, 0, 1], [1, 0, 1, 1, 1], [0, 1, 0, 1, 0], [0, 1, 1, 0, 1], [1, 1, 0, 1, 0]],
                          matrix.data)
