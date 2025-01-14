a9c7a4a2c60375f3a99a546a94eb7b83d18969c0
rubenhortas <rubenhortas@gmail.com>
2025-01-14 22:03:05 +0100

    Refactoring Dijkstra

--- data_structures_and_algorithms/algorithms/graphs/dijkstra/dijkstra.py
+++ data_structures_and_algorithms/algorithms/graphs/dijkstra/dijkstra.py
@@ -3,28 +3,26 @@ from data_structures_and_algorithms.data_structures.graphs.graph import Graph
 
 def get_shortest_path(graph: Graph, source_node: int, end_node: int) -> (list, float):
     distances = [float('inf')] * graph.num_nodes  # float('inf') represents positive infinity as a floating-point number
-    visited_nodes = [False] * graph.num_nodes
+    visited = {source_node}
     parents = [None] * graph.num_nodes
-    queue = []
+    queue = [source_node]
     distances[source_node] = 0
-    queue.append(source_node)
-    visited_nodes[source_node] = True
     index = 0
 
-    while index < len(queue) and not visited_nodes[end_node]:
+    while index < len(queue) and end_node not in visited:
         current_node = queue[index]
         index += 1
 
         # Update neighbors' distances
         _update_distances(graph, current_node, distances, parents)
 
-        # Find the first unvisited node with at the shortest distance
-        next_node = _pick_next_node(distances, visited_nodes)
+        # Find the first unvisited node at the shortest distance
+        next_node = _pick_next_node(distances, visited)
 
         if next_node:
             queue.append(next_node)
 
-        visited_nodes[current_node] = True
+        visited.add(current_node)
 
     return _get_path(parents, source_node, end_node), distances[end_node]
 
@@ -41,12 +39,12 @@ def _update_distances(graph: Graph, current_node: int, distances: list, parents:
             parents[node] = current_node
 
 
-def _pick_next_node(distances: list, visited_nodes: list) -> int:
+def _pick_next_node(distances: list, visited: set) -> int:
     min_distance = float('inf')
     min_node = None
 
     for node in range(len(distances)):
-        if not visited_nodes[node] and distances[node] < min_distance:
+        if node not in visited and distances[node] < min_distance:
             min_node = node
             min_distance = distances[node]
 
@@ -54,14 +52,14 @@ def _pick_next_node(distances: list, visited_nodes: list) -> int:
 
 
 def _get_path(parents: list, source_node, end_node: int) -> list:
-    path = []
+    if not parents:
+        return []
 
-    if parents:
-        path.append(end_node)
-        current_node = end_node
+    path = [end_node]
+    current_node = end_node
 
-        while current_node != source_node:
-            path.append(parents[current_node])
-            current_node = parents[current_node]
+    while current_node != source_node:
+        path.append(parents[current_node])
+        current_node = parents[current_node]
 
-        return path[::-1]
+    return path[::-1]
