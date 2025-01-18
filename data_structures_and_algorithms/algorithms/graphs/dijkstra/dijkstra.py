19729d84eaa92e8c3083e95ebd63a35199134959
rubenhortas <rubenhortas@gmail.com>
2025-01-18 21:02:12 +0100

    Refactored

--- data_structures_and_algorithms/algorithms/graphs/dijkstra/dijkstra.py
+++ data_structures_and_algorithms/algorithms/graphs/dijkstra/dijkstra.py
@@ -3,47 +3,47 @@ import heapq
 from data_structures_and_algorithms.data_structures.graphs.graph import Graph
 
 
-def get_shortest_path(graph: Graph, source_node: int, end_node: int) -> (list, float):
+def get_shortest_path(graph: Graph, start: int, end: int) -> (list, float):
     def update_distances():
-        neighbors = graph.nodes[current_node]
-        weights = graph.weights[current_node]
+        neighbors = graph.nodes[current]
+        weights = graph.weights[current]
 
         for i, node in enumerate(neighbors):
             weight = weights[i]
 
-            if distances[current_node] + weight < distances[node]:
-                distances[node] = distances[current_node] + weight
-                predecessors[node] = current_node
+            if distances[current] + weight < distances[node]:
+                distances[node] = distances[current] + weight
+                predecessors[node] = current
 
     distances = [float('inf')] * graph.num_nodes
     visited = set()
     predecessors = [None] * graph.num_nodes
     # Use a priority queue (min-heap) to efficiently select the next node
-    priority_queue = [(0, source_node)]  # (distance, node)
-    distances[source_node] = 0
+    priority_queue = [(0, start)]  # (distance, node)
+    distances[start] = 0
     path = []
 
     while priority_queue:
-        current_distance, current_node = heapq.heappop(priority_queue)
+        distance, current = heapq.heappop(priority_queue)
 
-        if current_node in visited:
+        if current in visited:
             continue  # Skip if already visited
 
-        visited.add(current_node)
+        visited.add(current)
 
-        if current_node == end_node:
+        if current == end:
             break
 
         update_distances()
 
-        for neighbor in graph.nodes[current_node]:
+        for neighbor in graph.nodes[current]:
             if neighbor not in visited:
                 heapq.heappush(priority_queue, (distances[neighbor], neighbor))
 
-    current = end_node
+    current = end
 
     while current is not None:
         path.insert(0, current)
         current = predecessors[current]
 
-    return path, distances[end_node]
+    return path, distances[end]
