e6d8eafefe1ceb7f34e590a602119ebeb4d86cc6
rubenhortas <rubenhortas@gmail.com>
2025-01-19 12:46:13 +0100

    Refactored

--- data_structures_and_algorithms/algorithms/sort/merge_sort/merge_sort.py
+++ data_structures_and_algorithms/algorithms/sort/merge_sort/merge_sort.py
@@ -1,6 +1,23 @@
 def sort(lst: list) -> list:
     # Time complexity: O(n*log(n))
     # Auxiliary space: O(n)
+    def merge() -> list:
+        result = []
+        left_index = 0
+        right_index = 0
+
+        while left_index < len(left_sorted) and right_index < len(right_sorted):
+            if left_sorted[left_index] <= right_sorted[right_index]:
+                result.append(left_sorted[left_index])
+                left_index += 1
+            else:
+                result.append(right_sorted[right_index])
+                right_index += 1
+
+        result.extend(left_sorted[left_index:])
+        result.extend(right_sorted[right_index:])
+
+        return result
 
     if len(lst) <= 1:
         return lst
@@ -8,29 +25,7 @@ def sort(lst: list) -> list:
     mid = len(lst) // 2
     left_half = lst[:mid]
     right_half = lst[mid:]
-
     left_sorted = sort(left_half)
     right_sorted = sort(right_half)
 
-    result = _merge(left_sorted, right_sorted)
-
-    return result
-
-
-def _merge(left, right) -> list:
-    result = []
-    left_index = 0
-    right_index = 0
-
-    while left_index < len(left) and right_index < len(right):
-        if left[left_index] <= right[right_index]:
-            result.append(left[left_index])
-            left_index += 1
-        else:
-            result.append(right[right_index])
-            right_index += 1
-
-    result.extend(left[left_index:])
-    result.extend(right[right_index:])
-
-    return result
+    return merge()
