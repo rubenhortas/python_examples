cdfb6b722eb87f167ff396f6c9b490e48b1ff418
rubenhortas <rubenhortas@gmail.com>
2025-01-25 00:40:41 +0100

    Refactored

--- data_structures_and_algorithms/data_structures/hashmap/linear_probing_hashtable.py
+++ data_structures_and_algorithms/data_structures/hashmap/linear_probing_hashtable.py
@@ -3,18 +3,7 @@ from data_structures_and_algorithms.data_structures.hashmap.basic_hashtable impo
 
 class LinearProbingHashTable(BasicHashTable):
     def _get_index(self, key: str) -> int:
-        def get_index():
-            result = 0
-
-            for character in key:
-                number = ord(character)
-                result += number
-
-            index = result % self._size
-
-            return index
-
-        index = get_index()
+        index = super()._get_index(key)
 
         while True:
             if self._data[index] is None or self._data[index][0] == key:
