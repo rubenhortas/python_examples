a43d2a6ebc9aa8e9d95cb7b1d3eec15167dab436
rubenhortas <rubenhortas@gmail.com>
2025-01-25 00:52:32 +0100

    Refactored

--- data_structures_and_algorithms/algorithms/binary_search/binary_search.py
+++ data_structures_and_algorithms/algorithms/binary_search/binary_search.py
@@ -5,28 +5,28 @@ def get_position(lst: list, number: int) -> int:
     :param number: 4
     :return: 5
     """
-    return _binary_search(lst, number, 0, len(lst) - 1)
 
+    def binary_search(lst: list, number: int, min_pos: int, max_pos: int) -> int:
+        # Time complexity: O(log(n))
+        # Auxiliary space: O(1)
+        def is_first_appearance(pos: int) -> bool:
+            prev_pos = pos - 1
 
-def _binary_search(lst: list, number: int, min_pos: int, max_pos: int) -> int:
-    # Time complexity: O(log(n))
-    # Auxiliary space: O(1)
-    def is_first_appearance(pos: int) -> bool:
-        prev_pos = pos - 1
+            return not (prev_pos > 0 and lst[prev_pos] == number)
 
-        return not (prev_pos > 0 and lst[prev_pos] == number)
+        if min_pos <= max_pos:
+            mid_pos = (min_pos + max_pos) // 2
 
-    if min_pos <= max_pos:
-        mid_pos = (min_pos + max_pos) // 2
-
-        if lst[mid_pos] > number:
-            return _binary_search(lst, number, (mid_pos + 1), max_pos)
-        elif lst[mid_pos] < number:
-            return _binary_search(lst, number, min_pos, (mid_pos - 1))
-        else:
-            if is_first_appearance(mid_pos):
-                return mid_pos
+            if lst[mid_pos] > number:
+                return binary_search(lst, number, (mid_pos + 1), max_pos)
+            elif lst[mid_pos] < number:
+                return binary_search(lst, number, min_pos, (mid_pos - 1))
             else:
-                return _binary_search(lst, number, min_pos, (mid_pos - 1))
-    else:
-        return -1
+                if is_first_appearance(mid_pos):
+                    return mid_pos
+                else:
+                    return binary_search(lst, number, min_pos, (mid_pos - 1))
+        else:
+            return -1
+
+    return binary_search(lst, number, 0, len(lst) - 1)
