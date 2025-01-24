884a5270a796cff9f259f2f96e57132feedba8ef
rubenhortas <rubenhortas@gmail.com>
2025-01-25 00:48:02 +0100

    Refactored

--- data_structures_and_algorithms/algorithms/binary_search/binary_search.py
+++ data_structures_and_algorithms/algorithms/binary_search/binary_search.py
@@ -11,6 +11,10 @@ def get_position(lst: list, number: int) -> int:
 def _binary_search(lst: list, number: int, min_pos: int, max_pos: int) -> int:
     # Time complexity: O(log(n))
     # Auxiliary space: O(1)
+    def is_first_appearance(pos: int) -> bool:
+        prev_pos = pos - 1
+
+        return not (prev_pos > 0 and lst[prev_pos] == number)
 
     if min_pos <= max_pos:
         mid_pos = (min_pos + max_pos) // 2
@@ -20,15 +24,9 @@ def _binary_search(lst: list, number: int, min_pos: int, max_pos: int) -> int:
         elif lst[mid_pos] < number:
             return _binary_search(lst, number, min_pos, (mid_pos - 1))
         else:
-            if _is_first_appearance(lst, number, mid_pos):
+            if is_first_appearance(mid_pos):
                 return mid_pos
             else:
                 return _binary_search(lst, number, min_pos, (mid_pos - 1))
     else:
         return -1
-
-
-def _is_first_appearance(lst: list, number: int, pos: int) -> bool:
-    prev_pos = pos - 1
-
-    return not (prev_pos > 0 and lst[prev_pos] == number)
