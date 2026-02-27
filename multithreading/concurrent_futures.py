73281ffe1303285b4d40a508bcf9ba240f5f802f
rubenhortas <rubenhortas@gmail.com>
2026-02-27 19:33:26 +0100

    refactor

--- multithreading/concurrent_futures.py
+++ multithreading/concurrent_futures.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import concurrent.futures
 import os
 import time
@@ -6,15 +8,15 @@ import urllib.request
 URLS = ["https://www.google.com/", "https://www.python.org/", "https://www.github.com/", "https://stackoverflow.com"]
 
 
-def load_url(url, timeout):
+def _load_url(url, timeout) -> bytes:
     with urllib.request.urlopen(url, timeout=timeout) as conn:
         return conn.read()
 
 
-def run_concurrent_tasks():
+def _main() -> None:
     # ThreadPoolExecutor for I/O bound tasks
     with concurrent.futures.ThreadPoolExecutor(max_workers=min(32, (os.cpu_count() or 1) + 4)) as executor:
-        future_to_url = {executor.submit(load_url, url, 60): url for url in URLS}
+        future_to_url = {executor.submit(_load_url, url, 60): url for url in URLS}
 
         for future in concurrent.futures.as_completed(future_to_url):
             url = future_to_url[future]
@@ -27,6 +29,9 @@ def run_concurrent_tasks():
 
 
 if __name__ == "__main__":
-    start_time = time.time()
-    run_concurrent_tasks()
-    print(f"Time: {time.time() - start_time:.2f}secs")
+    try:
+        start_time = time.time()
+        _main()
+        print(f"Time: {time.time() - start_time:.2f}secs")
+    except KeyboardInterrupt:
+        pass
