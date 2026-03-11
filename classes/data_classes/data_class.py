eb3212f6595519cc1f7edca0f65bbf9f8abd598a
rubenhortas <rubenhortas@gmail.com>
2026-03-11 21:03:23 +0100

    doc

--- classes/data_classes/data_class.py
+++ classes/data_classes/data_class.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
 
 """
- Since version 3.7, Python offers data classes.
+Since version 3.7, Python offers data classes.
 
- Advantages:
-    - requires a minimal amount of code
-    - __eq__ is implemented
-    - __repr__ is implemented (for print)
-    - requires type hints (reduces the chances of bugs)
+Advantages:
+   * Requires a minimal amount of code
+   * Automatically generated magic methods:
+       * __init__
+       * __eq__
+       * __repr__
+       * __hash__
+   * Requires type hints (reduces the chances of bugs)
+   * You can create immutable dataclasses by setting frozen=True
 """
 
 from dataclasses import dataclass
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
     @dataclass
     class Fighter:
         name: str
         suit: str
 
-
-    fighter1 = Fighter('Ryu', 'black')
-    fighter2 = Fighter('Ryu', 'black')
+    fighter1 = Fighter("Ryu", "black")
+    fighter2 = Fighter("Ryu", "black")
 
     print(fighter1 == fighter2)
     # return: True
