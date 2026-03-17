75c3d496d9364cba0b36035f7dafc19921d6fb3b
rubenhortas <rubenhortas@gmail.com>
2026-03-17 20:58:41 +0100

    refactor

--- classes/slots.py
+++ classes/slots.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+from dataclasses import dataclass
+
 """
 __slots__ reduces the memory footprint of instances.
 Instead of a dynamic dictionary to store attributes, Python uses a more compact structure.
@@ -10,12 +14,18 @@ Benefits of Using __slots__
 """
 
 
-class Hero:
-    __slots__ = ["hero_name", "name"]
-
-    def __init__(self, name: str, hero_name: str) -> None:
-        self.name = name
-        self.hero_name = hero_name
+# The 'slots=True' parameter (introduced in Python 3.10) automatically defines __slots__ based on the dataclass fields.
+@dataclass(slots=True)
+class _Hero:
+    # Example of manual __slots__ for older Python versions (< 3.10)
+    # __slots__ = ["hero_name", "name"]
+    hero_name: str
+    name: str
 
     def __str__(self) -> str:
-        return f"Hero(name={self.name}, hero_name={self.hero_name})"
+        return f"_Hero(name={self.name}, hero_name={self.hero_name})"
+
+
+if __name__ == "__main__":
+    hero = _Hero("Robbie Reyes", "Ghost Rider")
+    print(f"I'm {hero.name}, and I'm {hero.hero_name}")
