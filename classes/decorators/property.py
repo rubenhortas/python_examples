bddc2e6f9e2fea2c51175f256e0fabbf86fcccde
rubenhortas <rubenhortas@gmail.com>
2026-03-21 14:15:20 +0100

    feat: Add example

--- classes/decorators/property.py
+++ classes/decorators/property.py
@@ -5,11 +5,12 @@ from datetime import datetime
 
 class Book:
     def __init__(self, title: str, author: str, publication_year: int) -> None:
-        self.title = title
-        self.author = author
-        self.publication_year = publication_year
+        self.title: str = title
+        self.author: str = author
+        self.publication_year: int = publication_year
 
     # The @property decorator allows a method to be accessed like an attribute, enabling computed properties in a class.
+    # By default, it makes it read-only.
     @property
     def age(self) -> int:
         current_year = datetime.now().year
@@ -27,3 +28,10 @@ if __name__ == "__main__":
 
     print(f"The book is {book.age} years old.")
     # return: The book is X years old.
+
+    # Attempting to modify it will throw an error
+    try:
+        book.age = 1901
+        print(book.age)
+    except AttributeError:
+        print("You cannot change the publication year")
