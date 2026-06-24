e9ac5b5fc9794fcc6676befd71142662cf60b012
rubenhortas <rubenhortas@gmail.com>
2026-06-24 13:18:50 +0200

    syle: ruff

--- multiprocessing/concurrent_futures.py
+++ multiprocessing/concurrent_futures.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 import concurrent.futures
+import contextlib
 import math
 import os
 import time
 
-def _compute_factorial(n) -> str:
+
+def _compute_factorial(n: int) -> str:
     _ = math.factorial(n)
     return f"Factorial of {n} computed."
+
 
 def _main() -> None:
     numbers = [50000, 60000, 70000, 80000]
@@ -15,7 +18,7 @@ def _main() -> None:
     print(f"Starting execution with {len(numbers)} tasks...")
     start_time = time.perf_counter()
 
-    # By default, ProcessProolExecutor uses all CPU cores
+    # By default, ProcessPoolExecutor uses all CPU cores
     with concurrent.futures.ProcessPoolExecutor(max_workers=max(1, (os.cpu_count() or 1) - 1)) as executor:
         results = list(executor.map(_compute_factorial, numbers))
 
@@ -26,8 +29,7 @@ def _main() -> None:
 
     print(f"\nTotal time: {end_time - start_time:.2f} seconds")
 
+
 if __name__ == "__main__":
-    try:
+    with contextlib.suppress(KeyboardInterrupt):
         _main()
-    except KeyboardInterrupt:
-        pass
