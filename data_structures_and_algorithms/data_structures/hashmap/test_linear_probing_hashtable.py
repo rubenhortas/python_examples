27cc542f67c50319fa744e9206dac9ac7fd14445
rubenhortas <rubenhortas@gmail.com>
2026-07-08 16:46:06 +0200

    style: ruff

--- data_structures_and_algorithms/data_structures/hashmap/test_linear_probing_hashtable.py
+++ data_structures_and_algorithms/data_structures/hashmap/test_linear_probing_hashtable.py
@@ -1,22 +1,23 @@
 import unittest
 
+from data_structures_and_algorithms.data_structures.hashmap.key_not_found_error import KeyNotFoundError
 from data_structures_and_algorithms.data_structures.hashmap.linear_probing_hashtable import LinearProbingHashTable
 
 
 class Test(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.hashtable = LinearProbingHashTable(15)
         self.hashtable.insert("listen", "l")
         self.hashtable.insert("silent", "s")
         self.hashtable.insert("music", "m")
 
-    def test_list(self):
+    def test_list(self) -> None:
         self.hashtable.list()
 
-    def test_get_value(self):
+    def test_get_value(self) -> None:
         self.assertEqual("l", self.hashtable.get("listen"))
-        self.assertRaises(BaseException, self.hashtable.get, "play")
+        self.assertRaises(KeyNotFoundError, self.hashtable.get, "play")
 
-    def test_update(self):
+    def test_update(self) -> None:
         self.hashtable.update("music", "M")
         self.assertEqual("M", self.hashtable.get("music"))
