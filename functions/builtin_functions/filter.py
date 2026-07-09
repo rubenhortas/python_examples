4e8f86449473d1d8d8354759ba20c1b710d0401f
rubenhortas <rubenhortas@gmail.com>
2026-07-09 16:52:30 +0200

    style: ruff

--- functions/builtin_functions/filter.py
+++ functions/builtin_functions/filter.py
@@ -7,13 +7,11 @@ filter() return a new iterable with those items that meet the condition.
 
 
 def _is_even(number: int) -> bool:
-    if number % 2 == 0:
-        return True
-    return False
+    return number % 2 == 0
 
 
 if __name__ == "__main__":
-    numbers = [i for i in range(10)]
+    numbers = list(range(10))
 
     print(list(filter(_is_even, numbers)))
     # return: [0, 2, 4, 6, 8]
