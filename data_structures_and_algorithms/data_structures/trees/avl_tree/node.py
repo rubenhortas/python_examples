469b57f8cd6330a7a8bc6bd0d4cc088d2ade7ed2
rubenhortas <rubenhortas@gmail.com>
2026-07-09 13:44:19 +0200

    style: ruff

--- data_structures_and_algorithms/data_structures/trees/avl_tree/node.py
+++ data_structures_and_algorithms/data_structures/trees/avl_tree/node.py
@@ -1,12 +1,12 @@
 class Node:
-    def __init__(self, value):
+    def __init__(self, value: int) -> None:
         self.value = value
         self.left = None
         self.right = None
         self.height = 1
 
-    def __repr__(self):
-        return f"{self.value}, Left: {str(self.left)}, Right: {str(self.right)}, Height: {self.height}"
+    def __repr__(self) -> str:
+        return f"{self.value}, Left: {self.left!s}, Right: {self.right!s}, Height: {self.height}"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.value}"
