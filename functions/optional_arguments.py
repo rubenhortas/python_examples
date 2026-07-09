e132871fc6bda6563bd248a9e91cc38404b21538
rubenhortas <rubenhortas@gmail.com>
2026-07-09 16:53:22 +0200

    style: ruff

--- functions/optional_arguments.py
+++ functions/optional_arguments.py
@@ -2,7 +2,7 @@
 
 # Python does not support overloading, so if you want to overload a function this is the way.
 def _function(optional_argument: bool = True) -> None:
-    if optional_argument is True:
+    if optional_argument:
         print("Ok, it's true")
     else:
         print("It's false")
