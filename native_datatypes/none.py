e373adbf8d78ddebabf9d154817248957b22299a
rubenhortas <rubenhortas@gmail.com>
2026-07-09 17:24:29 +0200

    style: ruff

--- native_datatypes/none.py
+++ native_datatypes/none.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# ruff: noqa
+# noinspection PyComparisonWithNone
 
 """
 None is a null number.
@@ -9,12 +11,21 @@ You can assign None to any variable, but you can not create other NoneType objec
 """
 
 if __name__ == "__main__":
-    print("None:")
     print(f"None == False -> {None == False}")  # False
-    print(f"None == 0 -> {None == 0}")  # False
-    print(f"None == '' -> {None == ''}")  # False
-    print(f"None == None -> {None is None}")  # True
+    print(f"None == 0     -> {None == 0}")  # False
+    print(f"None == ''    -> {None == ''}")  # False
+    print(f"None == []    -> {None == []}")  # False
+
+    # noinspection PyComparisonWithNone
+    print(f"None == None  -> {None == None}")  # True
+    print(f"None is None  -> {None is None}")  # True
+    print(f"None != False -> {None != False}")  # True
+    print(f"None is not 0 -> {None is not 0}")  # True
 
     # In a boolean context None is False
     if not None:
         print("In a boolean context None is always False")
+
+    # noinspection PyStringConversionWithoutDunderMethod
+    print(f"type(None)           -> {type(None)}")
+    print(f"isinstance(None, type(None)) -> {isinstance(None, type(None))}")
